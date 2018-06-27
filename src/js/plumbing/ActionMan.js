5cfa6653592a0e5455588bf139aca77721ed0de6
Irina Preda <irinalaurapreda@gmail.com>
2018-06-27 13:06:13 +0100

    bringing branch up to speed

--- src/js/plumbing/ActionMan.js
+++ src/js/plumbing/ActionMan.js
@@ -11,24 +11,4 @@ import {getId, getType} from '../base/data/DataClass';
 
 import ActionMan from '../base/plumbing/ActionManBase';
 
-ActionMan.saveGlobal = (globId) => {
-	assMatch(globId, String);
-	let global = DataStore.getData(C.TYPES.Global, globId);
-	if ( ! global.id) {
-		assert(globId==='new');
-		global.id = globId;
-	}
-	let prevStatus = DataStore.getValue(['transient', globId, 'status']);
-	DataStore.setValue(['transient', globId, 'status'], C.STATUS.saving);
-	ServerIO.saveGlobal(global)
-	.then(DataStore.updateFromServer.bind(DataStore))
-	.fail((bleurgh) => {
-		console.warn("bleurgh", bleurgh);
-		DataStore.setValue(['transient', globId, 'status'], prevStatus);
-	})
-	.then(() => {
-		DataStore.setValue(['transient', globId, 'status'], C.STATUS.clean);
-	});
-};
-
 export default ActionMan;
