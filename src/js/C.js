634c9a161110e92b1b39627be3ad6b3513b124b4
Daniel Winterstein <daniel@winterwell.com>
2018-06-29 13:41:15 +0100

    sync

--- src/js/C.js
+++ src/js/C.js
@@ -12,7 +12,7 @@ C.app = {
 	service: "good-loop",
 	logo: "/img/logo.png",
 	facebookAppId: "320927325010346", // https://developers.facebook.com/apps/320927325010346/dashboard/
-	privacyPolicy: "",
+	privacyPolicy: "https://doc.good-loop.com/privacy-policy.html",
 	tsncs: "",
 };
 
