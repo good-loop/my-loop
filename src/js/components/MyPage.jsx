5980f2f233b75c2a290dbf7f1193e1c7eab3cb60
Irina Preda <irinalaurapreda@gmail.com>
2018-07-03 17:58:35 +0100

    fixed tracking id aggregation (construction of xids) and reorganised html of logged-in message

--- src/js/components/MyPage.jsx
+++ src/js/components/MyPage.jsx
@@ -26,16 +26,11 @@ const MyPage = () => {
 
 	let {xid, name} = Login.getUser() || {};
 
-	let trkIds = DataStore.getValue(trkIdPath);
-	// all the users IDs
-	let xids = trkIds || [];
-	if (Login.getId()) xids.push(Login.getId());
 	// fetch profile
 	let pvProfile = ActionMan.getProfile({xid});
-	let peep = pvProfile.value || {};	
-	// linked IDs?
-	let linkedIds = Person.linkedIds(peep);
-	xids = xids.concat(linkedIds);
+	let peep = pvProfile.value || {};
+
+	let trkIds = DataStore.getValue(trkIdPath);
 
 	if ( ! trkIds) {
 		// User is logged in but we haven't retrieved tracking IDs from shares yet		
@@ -45,12 +40,22 @@ const MyPage = () => {
 			// if (currentTrkId && !trkIds.includes(currentTrkId)) {
 			// 	ServerIO.putProfile({id: uid, [FIELDS.trackIds]: trkIds.concat(currentTrkId)});
 			// }
-			// // put them in datastore whether we've updated profile or not
-			// DataStore.setValue(trkIdPath, trkIds);	
+			
 		});
 		// use the current one?
 		if (currentTrkId) trkIds=[currentTrkId];
+		// put them in datastore whether we've updated profile or not
+		DataStore.setValue(trkIdPath, trkIds);
 	}
+
+
+	// all the users IDs
+	let xids = trkIds || [];
+	if (Login.getId()) xids.push(Login.getId());
+
+	// linked IDs?
+	let linkedIds = Person.linkedIds(peep);
+	xids = xids.concat(linkedIds);
 
 	// display...
 	return (
@@ -58,7 +63,7 @@ const MyPage = () => {
 			<Misc.Card>
 				<WelcomeCard currentTrkId={currentTrkId} trkIds={trkIds} />
 			</Misc.Card>
-			<MyReport uid={xid} xids={xids} />
+			<MyReport uid={xid} xids={xids} trkids={trkIds} />
 		</div>
 	);
 }; // ./DashboardPage
@@ -66,18 +71,16 @@ const MyPage = () => {
 
 const WelcomeCard = ({trkIds}) => {
 	return (<div className="header">
-		
+		{Login.isLoggedIn()? 
+			<div className="pull-right logged-in">
+				<p>Hi { Login.getUser().name || Login.getUser().xid }</p>
+				<small className="pull-right"><a href="#my" onClick={e => stopEvent(e) && Login.logout()}>Log out</a></small>
+			</div>
+			: <LoginLink className='btn btn-lg btn-red' />				
+		}
 		<div className="header-text">
 			<p className="title">TAKE CONTROL OF YOUR DATA</p>
 			<p className="subtitle">You choose what data you give us<br/> and what we do with it.</p>
-			
-			{Login.isLoggedIn()? 
-				<div>
-					<p>You are logged in as { Login.getUser().name || Login.getUser().xid }.</p>
-					<small><a href="#my" onClick={e => stopEvent(e) && Login.logout()}>Log out</a></small>
-				</div>
-				: <LoginLink className='btn btn-lg btn-red' />				
-			}
 		</div>
 	</div>);
 };
