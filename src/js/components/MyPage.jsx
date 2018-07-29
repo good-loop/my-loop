8487aa8bb997c217e54b3511ffa2a8f1b051d510
Daniel Winterstein <daniel@sodash.com>
2018-07-29 17:08:53 +0100

    no-social

--- src/js/components/MyPage.jsx
+++ src/js/components/MyPage.jsx
@@ -80,6 +80,8 @@ const MyPage = () => {
 	// HACK DataLog query string: "user:trkid1@trk OR user:trkid2@trk OR ..."
 	const allIds = xids.map(trkid => 'user:' + trkid).join(' OR ');
 
+// TODO
+				// <Card title='Boost Your Impact' defaultOpen><SocialMediaCard allIds={xids} /></Card>
 	// display...
 	return (
 		<div className="page MyPage">
@@ -94,8 +96,6 @@ const MyPage = () => {
 				<Card title='Your Donations' defaultOpen><DonationCard xids={xids} /></Card>
 
 				<Card title='Consent Controls' defaultOpen>{Login.isLoggedIn()? <ConsentWidget xids={xids} /> : <LoginToSee />}</Card>
-
-				{true? null : <Card title='Boost Your Impact' defaultOpen><SocialMediaCard allIds={xids} /></Card>}
 
 				<Card title='Get In Touch' defaultOpen><ContactCard allIds={allIds} /></Card>
 				
