c3adaa4472432116ac13d96a223d690d6894124c
Irina Preda <irinalaurapreda@gmail.com>
2018-08-22 18:26:22 +0100

    added images and fonts

--- src/js/components/CampaignPage.jsx
+++ src/js/components/CampaignPage.jsx
@@ -31,53 +31,64 @@ const CampaignPage = () => {
 	return (<div className="campaign-page">
 		<div className="wrapper">
 			<div className="one">
-				<p className="title">
-					£15,563.80<br/> 
-					RAISED SO FAR
+				<p className="kitkat frank-font">
+					<img alt="Kitkat Logo" src="/img/kitkat-logo.png" />
+					<span> Supports </span>
+					<img alt="Cocoa Plan Logo" src="/img/cocoa-plan-logo.png" />
 				</p>
-				<div className="arrow">
-					<Link activeClass="active" className="arrowhead" to="arrowhead" spy={true} smooth={true} duration={2000}>
-							<span></span>
-							<span></span>
-					</Link>
-				</div>
+				<div className="header-img">
+					<div className="darken-overlay">
+						<p className="title frank-font">
+							<p>£15,563.80</p>
+							<p>RAISED SO FAR</p>
+						</p>
+						<p className="ads-for-good">
+							ADS FOR GOOD 
+						</p>
+						<div className="arrow">
+							<Link activeClass="active" className="arrowhead" to="arrowhead" spy={true} smooth={true} duration={2000}>
+									<span></span>
+									<span></span>
+							</Link>
+						</div>
+					</div>	
+  				</div>
 			</div>
 			<div className="two">
 				<Element name="arrowhead" className="element">
 					<div className="inside">
-						<p className="title">
+						<p className="title frank-font">
 							OUR COMMITMENT
 						</p>
-						<p className="subtitle">
+						<p className="subtitle helvetica-font">
 							At Nestlé we’re committed to improving the lives of cocoa farmers.<br/>
 					So we have committed to donating half of our KITKAT® advertising money to
 					support the NESTLÉ® COCOA PLAN®, through the ‘Ads for Good’ player.
 						</p>
-						<p className="link">
-							<a href=''>
+						<p className="link bebas-font">
+							<a href='http://as.good-loop.com/'>
 							WATCH AN ADVERT, UNLOCK A FREE DONATION, AND CHOOSE WHICH NESTLÉ® COCOA PLAN® PROJECT YOU WOULD LIKE TO FUND.
 							</a>
 						</p>
 						<div className="donation-circles">
 							<div className="circle">
-								<p><span>26%</span><br/> HAS BEEN DONATED TO...</p>
-								<img src="https://www.musthavegifts.org/media/catalog/product/cache/1/small_image/400x400/17f82f742ffe127f42dca9de82fb58b1/d/4/d485-0770-36_870901_1200x1200.jpg" />
-								<div className="charity-name">
+								<p className="bebas-font"><span className="frank-font">26%</span><br/> HAS BEEN DONATED TO...</p>
+								<img alt="Vegetable Growing Kit" src="/img/stats1.jpg" />
+								<div className="project-name frank-font">
 									Vegetable Growing Kit
 								</div>
 							</div>
 							<div className="circle">
-								<p><span>36%</span><br/> HAS BEEN DONATED TO...</p>
-								<img src="https://www.musthavegifts.org/media/catalog/product/cache/1/small_image/400x400/17f82f742ffe127f42dca9de82fb58b1/d/4/d485-0770-36_870901_1200x1200.jpg" />
-								<div className="charity-name">
+								<p className="bebas-font"><span className="frank-font">36%</span><br/> HAS BEEN DONATED TO...</p>
+								<img alt="Solar Chargers" src="/img/stats2.jpg" />
+								<div className="project-name frank-font">
 									Solar Chargers
 								</div>
 							</div>
 							<div className="circle">
-								<p><span>
-								38%</span><br/> HAS BEEN DONATED TO...</p>
-								<img src="https://www.musthavegifts.org/media/catalog/product/cache/1/small_image/400x400/17f82f742ffe127f42dca9de82fb58b1/d/4/d485-0770-36_870901_1200x1200.jpg" />
-								<div className="charity-name">
+								<p className="bebas-font"><span className="frank-font">38%</span><br/> HAS BEEN DONATED TO...</p>
+								<img alt="School Kits" src="/img/stats3.jpg" />
+								<div className="project-name frank-font">
 									School Kits
 								</div>
 							</div>
@@ -85,7 +96,7 @@ const CampaignPage = () => {
 					</div>
 				</Element>
 			</div>
-			<div className="four">
+			<div className="four bebas-font">
 				<div className="foot">
 					<div className="social">
 						<p>KITKAT®</p>
