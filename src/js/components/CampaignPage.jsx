b1fd295c4cbc99098c22777b6a2e6a10040a0801
Irina Preda <irinalaurapreda@gmail.com>
2018-08-23 14:49:55 +0100

    added project details widget

--- src/js/components/CampaignPage.jsx
+++ src/js/components/CampaignPage.jsx
@@ -19,7 +19,7 @@ import {getProfile, getProfilesNow} from '../base/Profiler';
 import ConsentWidget from './ConsentWidget';
 import printer from '../base/utils/printer';
 import DonationCard from './DonationCard';
-import { Link, Element } from "react-scroll";
+import { Link, Element } from 'react-scroll';
 
 const CampaignPage = () => {
 	let campaignPage = {
@@ -28,25 +28,25 @@ const CampaignPage = () => {
 	// TODO advert & campaign from the path
 	const lpath = DataStore.getValue(['location','path']);
 	let vertId = lpath[1];
-	return (<div className="campaign-page">
-		<div className="wrapper">
-			<div className="one">
-				<p className="kitkat frank-font">
-					<img alt="Kitkat Logo" src="/img/kitkat-logo.png" />
+	return (<div className='campaign-page'>
+		<div className='wrapper'>
+			<div className='one'>
+				<p className='kitkat frank-font'>
+					<img alt='Kitkat Logo' src='/img/kitkat-logo.png' />
 					<span> Supports </span>
-					<img alt="Cocoa Plan Logo" src="/img/cocoa-plan-logo.png" />
+					<img alt='Cocoa Plan Logo' src='/img/cocoa-plan-logo.png' />
 				</p>
-				<div className="header-img">
-					<div className="darken-overlay">
-						<p className="title frank-font">
+				<div className='header-img'>
+					<div className='darken-overlay'>
+						<p className='title frank-font'>
 							<p>£15,563.80</p>
 							<p>RAISED SO FAR</p>
 						</p>
-						<p className="ads-for-good">
-							ADS FOR GOOD 
+						<p className='ads-for-good'>
+							<img alt='Good Loop Ads For Good Logo' src='/img/ads-for-good.png' />
 						</p>
-						<div className="arrow">
-							<Link activeClass="active" className="arrowhead" to="arrowhead" spy={true} smooth={true} duration={2000}>
+						<div className='arrow'>
+							<Link activeClass='active' className='arrowhead' to='arrowhead' spy={true} smooth={true} duration={2000}>
 									<span></span>
 									<span></span>
 							</Link>
@@ -54,67 +54,90 @@ const CampaignPage = () => {
 					</div>	
   				</div>
 			</div>
-			<div className="two">
-				<Element name="arrowhead" className="element">
-					<div className="inside">
-						<p className="title frank-font">
+			<div className='two'>
+				<Element name='arrowhead' className='element'>
+					<div className='inside'>
+						<p className='title frank-font'>
 							OUR COMMITMENT
 						</p>
-						<p className="subtitle helvetica-font">
+						<p className='subtitle helvetica-font'>
 							At Nestlé we’re committed to improving the lives of cocoa farmers.<br/>
 					So we have committed to donating half of our KITKAT® advertising money to
 					support the NESTLÉ® COCOA PLAN®, through the ‘Ads for Good’ player.
 						</p>
-						<p className="link bebas-font">
-							<a href='http://as.good-loop.com/'>
+						<p className='link bebas-font'>
+							<a href='http://as.good-loop.com/' target='_blank'>
 							WATCH AN ADVERT, UNLOCK A FREE DONATION, AND CHOOSE WHICH NESTLÉ® COCOA PLAN® PROJECT YOU WOULD LIKE TO FUND.
 							</a>
 						</p>
-						<div className="donation-circles">
-							<div className="circle">
-								<p className="bebas-font"><span className="frank-font">26%</span><br/> HAS BEEN DONATED TO...</p>
-								<img alt="Vegetable Growing Kit" src="/img/stats1.jpg" />
-								<div className="project-name frank-font">
+						<div className='donation-circles'>
+							<div className='circle'>
+								<p className='bebas-font'><span className='frank-font'>26%</span><br/> HAS BEEN DONATED TO...</p>
+								<img alt='Vegetable Growing Kit' src='/img/stats1.jpg' />
+								<div className='project-name frank-font'>
 									Vegetable Growing Kit
 								</div>
 							</div>
-							<div className="circle">
-								<p className="bebas-font"><span className="frank-font">36%</span><br/> HAS BEEN DONATED TO...</p>
-								<img alt="Solar Chargers" src="/img/stats2.jpg" />
-								<div className="project-name frank-font">
+							<div className='circle'>
+								<p className='bebas-font'><span className='frank-font'>36%</span><br/> HAS BEEN DONATED TO...</p>
+								<img alt='Solar Chargers' src='/img/stats2.jpg' />
+								<div className='project-name frank-font'>
 									Solar Chargers
 								</div>
 							</div>
-							<div className="circle">
-								<p className="bebas-font"><span className="frank-font">38%</span><br/> HAS BEEN DONATED TO...</p>
-								<img alt="School Kits" src="/img/stats3.jpg" />
-								<div className="project-name frank-font">
+							<div className='circle'>
+								<p className='bebas-font'><span className='frank-font'>38%</span><br/> HAS BEEN DONATED TO...</p>
+								<img alt='School Kits' src='/img/stats3.jpg' />
+								<div className='project-name frank-font'>
 									School Kits
 								</div>
 							</div>
 						</div>
 					</div>
 				</Element>
+				<div className="arrow-up"></div>
 			</div>
-			<div className="four bebas-font">
-				<div className="foot">
-					<div className="social">
+			<div className='three'>
+				<div className='details'>
+					<div className='top bebas-font'>
+						MORE INFORMATION ABOUT THIS CAUSE
+					</div>
+					<div className='title frank-font'>Vegetable Growing Kit</div>
+					<div className='description helvetica-font'>
+						Lorem ipsum dolor sit amet, consectetuer adipiscing
+						elit, sed diam nonummy Lorem ipsum dolor sit amet,
+						consectetuer adipiscing elit, sed diam nonummy nibh
+						euismod tincidunt ut laoreet dolore magna aliquam erat
+						volutpat. Ut wisi enim ad minim veniam, quis nostrud
+						exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+						ex ea commodo consequat.
+					</div>
+					<div className='link frank-font'>
+						<a href="http://www.nestlecocoaplan.com/">
+							Find out more about the<br/> Nestlé® Cocoa Plan®
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className='four bebas-font'>
+				<div className='foot'>
+					<div className='social'>
 						<p>KITKAT®</p>
-						<img src="https://i.imgur.com/6JwS1Tj.png"/>
-						<img src="https://i.imgur.com/sLaWazJ.png"/>
-						<img src="https://i.imgur.com/5rDki1U.png"/>
+						<img src='https://i.imgur.com/6JwS1Tj.png'/>
+						<img src='https://i.imgur.com/sLaWazJ.png'/>
+						<img src='https://i.imgur.com/5rDki1U.png'/>
 					</div>
-					<div className="social">
+					<div className='social'>
 						<p>NESTLÉ® COCOA PLAN®</p>
-						<img src="https://i.imgur.com/6JwS1Tj.png"/>
-						<img src="https://i.imgur.com/sLaWazJ.png"/>
-						<img src="https://i.imgur.com/5rDki1U.png"/>
+						<img src='https://i.imgur.com/6JwS1Tj.png'/>
+						<img src='https://i.imgur.com/sLaWazJ.png'/>
+						<img src='https://i.imgur.com/5rDki1U.png'/>
 					</div>
-					<div className="social">
+					<div className='social'>
 						<p>GOOD LOOP</p>
-						<img src="https://i.imgur.com/6JwS1Tj.png"/>
-						<img src="https://i.imgur.com/sLaWazJ.png"/>
-						<img src="https://i.imgur.com/5rDki1U.png"/>
+						<img src='https://i.imgur.com/6JwS1Tj.png'/>
+						<img src='https://i.imgur.com/sLaWazJ.png'/>
+						<img src='https://i.imgur.com/5rDki1U.png'/>
 					</div>
 				</div>
 			</div>
