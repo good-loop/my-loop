9f4ab0f7e03e1cc32be94c28bbf3e01df45d57c6
DA <daniel.appel.winterwell@gmail.com>
2018-10-01 18:20:54 +0100

    changed where the youtube and facebook icons are pulled from

--- src/js/components/CampaignPage.jsx
+++ src/js/components/CampaignPage.jsx
@@ -41,10 +41,10 @@ const SocialMediaFooterWidget = ({type, name, src}) => {
 	return (
 		<div className={'social '.concat(type)}>
 			<MDText source={name} />
-			{src && src.fb_url? <a href={src.fb_url} target='_blank'><img src='https://gl-es-03.good-loop.com/cdn/images/facebook.png' /></a> : null}
+			{src && src.fb_url? <a href={src.fb_url} target='_blank'><img src='https://gl-es-05.good-loop.com/cdn/images/facebook.png' /></a> : null}
 			{src && src.tw_url? <a href={src.tw_url} target='_blank'><img src='https://gl-es-04.good-loop.com/cdn/images/twitter.png' /></a> : null}
 			{src && src.insta_url? <a href={src.insta_url} target='_blank'><img src='https://gl-es-05.good-loop.com/cdn/images/instagram.png' /></a> : null}
-			{src && src.yt_url? <a href={src.yt_url} target='_blank'><img src='https://gl-es-03.good-loop.com/cdn/images/youtube.png' /></a> : null}
+			{src && src.yt_url? <a href={src.yt_url} target='_blank'><img src='https://gl-es-04.good-loop.com/cdn/images/youtube.png' /></a> : null}
 		</div>
 	);
 };
