57817a67703d805cb6d4a1e1d54c5822a2fbbb2a
Mark Wood <markw13a@gmail.com>
2018-11-19 13:51:00 +0000

    Files required to build GoodLoop app

---
+++ build-app.sh
@@ -0,0 +1,12 @@
+# Packages MyLoop into a zip file that can then be uploaded to
+# https://build.phonegap.com/apps/3373427/builds
+
+# Safety: delete any previously generated zip files
+# if you don't do this, new zip created will contain the old zip file
+# Means that file size grows every time that this script is run
+rm myloop.zip
+mkdir ~/winterwell/myloopapp
+# PhoneGap requires that app files be wrapped in top-level directory named "www"
+cp -r ~/winterwell/my-loop ~/winterwell/myloopapp/www
+zip -r myloop.zip ~/winterwell/myloopapp/www
+rm -r -f ~/winterwell/myloopapp
