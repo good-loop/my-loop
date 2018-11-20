84593e0180ead8ec9d70042adfd89ea0fc504869
Mark Wood <markw13a@gmail.com>
2018-11-20 11:59:31 +0000

    Rebundles before creating .zip. Realised that the app only needs the web folder. Significantly reduced size

--- build-app.sh
+++ build-app.sh
@@ -5,8 +5,9 @@
 # if you don't do this, new zip created will contain the old zip file
 # Means that file size grows every time that this script is run
 rm myloop.zip
+npm run compile
 mkdir ~/winterwell/myloopapp
 # PhoneGap requires that app files be wrapped in top-level directory named "www"
-cp -r ~/winterwell/my-loop ~/winterwell/myloopapp/www
+cp -r ~/winterwell/my-loop/web ~/winterwell/myloopapp/www
 zip -r myloop.zip ~/winterwell/myloopapp/www
 rm -r -f ~/winterwell/myloopapp
