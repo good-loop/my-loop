99cad49637d370ee6b46a6dfd9c653b728401ef3
Mark Wood <markw13a@gmail.com>
2019-03-04 10:53:35 +0000

    Now a functional component. Remains an issue with ShareAnAd not always showing last watched ad. Cause is understood, will file a bug

--- src/js/components/ShareAnAd.jsx
+++ src/js/components/ShareAnAd.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import md5 from 'md5';
 import DataStore from '../base/plumbing/DataStore';
 import ServerIO from '../plumbing/ServerIO';
@@ -8,30 +8,33 @@ import {saveSocialShareId} from '../base/Profiler';
 import GoodLoopUnit from '../base/components/GoodLoopUnit';
 import {withLogsIfVisible} from '../base/components/HigherOrderComponents';
 
-// TODO Does this need to be a component? If not, avoid React.Component in favour of functional jsx
-// (18/02/19) Should be possible now that GoodLoopUnit is inserted elsewhere, but still need div reference is order to report
-// when a div becomes visible.
-
-const ShareAnAd = ({ adHistory={}, logsIfVisibleRef }) => {
-	let {vert, format, video} = adHistory;
-
+/**
+ * Shows: 
+ * 1) A preview of a GoodLoop ad. This is either the last ad watched by the user or a random ad
+ * 2) A Twitter intent link to share this ad
+ * 3) A table showing how many times their shared ads have been viewed by others
+ */
+const ShareAnAd = ({ adHistory, logsIfVisibleRef }) => {
+	// Load in back-up vert data
+	// Easiest to just always load back-up data:
+	// avoids a race-condition where adHistory is provided after initial render has set off fetch
+	// Could mean that backup data is always applied as promise resolves and overrides data passed via adHistory
+	const [backUpVertData, setBackUpVertData] = useState({});
 	useEffect( () => {
-		if( !vert ) {
-			ServerIO.load(ServerIO.AS_ENDPOINT + '/unit.json', {swallow:true})
-				.then( res => {
-					const vertData = res.vert;
+		ServerIO.load(ServerIO.AS_ENDPOINT + '/unit.json', {swallow:true})
+			.then( res => {
+				if( !res.vert ) {
+					console.warn("Unit.json not returning any advert data?");
+					return;
+				}
 
-					if( !vertData ) {
-						console.warn("Unit.json not returning any advert data?");
-						return;
-					}
-
-					vert = vertData.adid;
-					video = vertData.video;
-					format = vertData.format;
+				setBackUpVertData({
+					vert: res.vert.id,
+					...(res.vert.videos && res.vert.videos[0] || {}),
 				});
-		}
-	}, [vert]);
+			});
+	}, []);
+	let {vert, format, video} = adHistory || backUpVertData || {};
 
 	const twitterXId = Person.getTwitterXId();
 
@@ -43,30 +46,68 @@ const ShareAnAd = ({ adHistory={}, logsIfVisibleRef }) => {
 					? <video controls={true} width="100%" height="auto" src={video}> An error occured </video> 
 					: <GoodLoopUnit adID={vert} /> 
 			}
-			<TwitterShare adID={vert} TwitterXId={twitterXId} />
-			<SharedAdsDisplay xid={twitterXId} />
+			{
+				twitterXId
+				&& (
+					<div>
+						<TwitterIntentLink twitterXId={twitterXId} vert={vert} />
+						<SharedAdStats twitterXId={twitterXId} />
+					</div>
+				)
+			}
 		</div>
 	);
 };
 
-/** 
- * Table with data on ads shared by the user
- * Data shown: Name of Advert	Number of times somebody has viewed ad shared by user	  Date Shared
- * A unique tracking ID is used to determine how many people have watched an ad as a result of it being shared
+/** Do we want to make this a generic component? Feels like it should be
+ * Twitter share button: user clicks and is prompted to tweet a link to one of our ads
  */
-const SharedAdsDisplay = ({xid}) => {
-	const twitterSocialShareObjects = xid ? DataStore.getValue(['data', 'Person', xid, 'socialShareIds']) : null;
+const TwitterIntentLink = ({vert, twitterXId}) => {
+	// Allows us to track stats for ads shared by individual users
+	const socialShareId = md5( twitterXId + vert );
+	console.warn("IntentLink", vert);
+	return (
+		<a 
+			href={"https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fas.good-loop.com%2F&amp;ref_src=twsrc%5Etfw&amp;text=I%20just%20gave%20to%20charity%20by%20watching%20a%20%40GoodLoopHQ%20ad%20%3A)&amp;tw_p=tweetbutton&amp;url=https%3A%2F%2Fas.good-loop.com%2F%3Fgl.vert%3D" + encodeURIComponent(vert + "&gl.socialShareId=" + socialShareId)} 
+			className="btn tweet-button" 
+			id="b"
+			target="_blank"
+			rel="noreferrer"
+			onClick={() => saveSocialShareId({xid: twitterXId, socialShareId, adid:vert})}
+		>
+			<span className='fa fa-twitter' />
+			<span className="label" id="l">Tweet</span>
+		</a>
+	);
+};
+
+/** Table showing how many times adverts shared by the user on Twitter have been viewed */
+const SharedAdStats = ({twitterXId}) => {
+	const twitterSocialShareObjects = twitterXId && DataStore.getValue(['data', 'Person', twitterXId, 'socialShareIds']);
 	if( !twitterSocialShareObjects ) return null;
 
-	// Want an array of just the ID strings. Filter out other crap
-	const shareIds = twitterSocialShareObjects.map( shareIdObject => shareIdObject.id);
 	// Load number of times that shared ad has been viewed
-	DataStore.fetch(['data', 'Person', xid, 'views'], () => ServerIO.getViewCount(shareIds));
+	let views={};
+	useEffect(() => {
+		const shareIds = twitterSocialShareObjects.map( shareIdObject => shareIdObject.id);
+		views = ServerIO.getViewCount(shareIds);
+	}, [twitterSocialShareObjects]);
+
 	// Load human-readable name for each shared ad
-	twitterSocialShareObjects.forEach( shareObject => {
-		const {adId, id} = shareObject;
-		DataStore.fetch(['data', 'Person', xid, 'adData', id], () => ServerIO.getVertData(adId));
-	});
+	const [names, setNames] = useState({});
+	useEffect(() => {
+		console.warn(twitterSocialShareObjects);
+		twitterSocialShareObjects
+			.forEach( shareObject => {
+				ServerIO.getVertData(shareObject.adId)
+					.then( res => {
+						const { cargo={} } = res;
+						const {id, name} = cargo;
+						// Append to names only if an ad id and name have been returned
+						return id && name && setNames({...names, [id]: name}); 
+					});
+			});
+	}, [twitterSocialShareObjects]);
 
 	return (
 		<div className='sharedAds container-fluid'>
@@ -82,13 +123,11 @@ const SharedAdsDisplay = ({xid}) => {
 				<tbody>
 					{twitterSocialShareObjects.map( shareIdObj => {
 						const {id, adId, dateShared} = shareIdObj;
-						const views = DataStore.getValue(['data', 'Person', xid, 'views', id]);
-						const name = DataStore.getValue(['data', 'Person', xid, 'adData', id, 'name']);
 
 						return (
-							<tr key='id'>
-								<td> {name || adId} </td>
-								<td> {views} </td>
+							<tr key={id}>
+								<td> {names[adId] || adId} </td>
+								<td> {views[id] || 0} </td>
 								<td> {dateShared} </td>
 							</tr>
 						);
@@ -97,26 +136,6 @@ const SharedAdsDisplay = ({xid}) => {
 			</table>
 		</div>
 	);
-};
-
-const TwitterShare = ({adID, TwitterXId}) => {
-	const TwitterSocialShareId = md5( TwitterXId + adID );
-
-	const onClick = () => saveSocialShareId({xid: TwitterXId, socialShareId: TwitterSocialShareId, adID});
-
-	// ??maybe just use gl.via=uxid + a post nonce instead of the social share id machinery??
-
-	return (
-		<a href={"https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fas.good-loop.com%2F&amp;ref_src=twsrc%5Etfw&amp;text=I%20just%20gave%20to%20charity%20by%20watching%20a%20%40GoodLoopHQ%20ad%20%3A)&amp;tw_p=tweetbutton&amp;url=https%3A%2F%2Fas.good-loop.com%2F%3Fgl.vert%3D" + encodeURIComponent(adID + "&gl.socialShareId=" + TwitterSocialShareId)} 
-			className="btn tweet-button" 
-			id="b"
-			target="_blank"
-			rel="noreferrer"
-			onClick={onClick}
-		>
-			<span className='fa fa-twitter' />
-			<span className="label" id="l">Tweet</span>
-		</a>);
 };
 
 export default withLogsIfVisible(ShareAnAd);
