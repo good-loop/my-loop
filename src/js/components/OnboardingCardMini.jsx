54890b03e1d3d89a7b37656f159bf8891d04a5d0
Mark Wood <markw13a@gmail.com>
2019-03-29 15:53:08 +0000

    Moved images to be hosted by us instead

--- src/js/components/OnboardingCardMini.jsx
+++ src/js/components/OnboardingCardMini.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 
 const OnboardingCardMini = () => {
-	const step1Img = 'https://image.ibb.co/nnGOgV/153970313184640369.png';
-	const step2Img = 'https://image.ibb.co/jJm3Fq/153970315675413631.png';
-	const step3Img = 'https://image.ibb.co/fMRQTA/153970316087031793.png';
-
 	return (
 		<section id="howitworks" className="how text-center">
 			<div className="how-content container-fluid">
 				<div className="row">
 					<div className="col-md-4 how-step">
-						<img className="how-img" src={step1Img} alt='banners in a web page' />
+						<img className="how-img" src='/img/step1.png' alt='banners in a web page' />
 						<span className="how-text">You see one of our Ads For Good on a website</span>
 					</div>
 					<div className="col-md-4 how-step">
-						<img className="how-img" src={step2Img} alt='banners in a web page' />
+						<img className="how-img" src='/img/step2.png' alt='banners in a web page' />
 						<span className="how-text">A video ad plays for 15 seconds</span>
 					</div>
 					<div className="col-md-4 how-step">
-						<img className="how-img" src={step3Img} alt='banners in a web page' />
+						<img className="how-img" src='/img/step3.png' alt='banners in a web page' />
 						<span className="how-text">We donate half the ad revenue to your chosen charity</span>
 					</div>
 				</div>
