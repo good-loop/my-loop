50bd20e0494ea98b166be5fb97fe063382f0ae5b
Mark Wood <markw13a@gmail.com>
2019-04-09 13:51:57 +0100

    Slightly changed onboarding wording. Kind of want to do something about the images

--- src/js/components/OnboardingCardMini.jsx
+++ src/js/components/OnboardingCardMini.jsx
@@ -7,15 +7,15 @@ const OnboardingCardMini = () => {
 				<div className="row">
 					<div className="col-md-4 how-step">
 						<img className="how-img" src='/img/step1.png' alt='banners in a web page' />
-						<span className="how-text">You see one of our Ads For Good on a website</span>
+						<span className="how-text"> You click on one of our Ads For Good </span>
 					</div>
 					<div className="col-md-4 how-step">
 						<img className="how-img" src='/img/step2.png' alt='banners in a web page' />
-						<span className="how-text">A video ad plays for 15 seconds</span>
+						<span className="how-text"> You watch a 15 second video ad </span>
 					</div>
 					<div className="col-md-4 how-step">
 						<img className="how-img" src='/img/step3.png' alt='banners in a web page' />
-						<span className="how-text">We donate half the ad revenue to your chosen charity</span>
+						<span className="how-text"> We donate 50% of the ad revenue to your chosen charity </span>
 					</div>
 				</div>
 				<div className="row">
