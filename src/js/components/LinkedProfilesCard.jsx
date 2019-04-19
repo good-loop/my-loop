e2ca6a0a9ba29059cc57793519ee973f9a9cf392
Mark Wood <markw13a@gmail.com>
2019-04-19 16:09:45 +0100

    Fixed broken-looking layout

--- src/js/components/LinkedProfilesCard.jsx
+++ src/js/components/LinkedProfilesCard.jsx
@@ -20,13 +20,46 @@ const LinkedProfilesCard = ({xids}) => {
 		<div>
 			<p>We all have multiple online identities -- e.g. emails, social media, and with retail companies. 
 			Here are the IDs Good-Loop recognises as you:</p>
-			<div className='word-wrap'>
-				{ nonTrackers.map(xid => <div key={xid}>{XId.service(xid)+': '+XId.id(xid)}</div>) }
-				Good-Loop cookies (random IDs, used by us to record your donations and avoid repeating ads): {trackers.map(xid => XId.id(xid)).join(", ")}<br/>
-				Currently logged into Good-Loop via: {authd.map(xid => XId.service(xid)+': '+XId.id(xid)).join(", ")}<br/>
-				Links: {peeps.map(peep => 
-					<div key={Person.id(peep)}>{Person.id(peep)} -> {peep.links && peep.links.length? peep.links.map(link => link.v).join(", ") : 'no links'}</div>
-				)}
+			<div>
+				<div className='word-wrap'>
+					{ nonTrackers.map(xid => <>
+						<div key={xid}>
+							{XId.service(xid)+': '+XId.id(xid)}
+						</div>
+						<br />
+					</>) }
+				</div>
+				<div>
+					Good-Loop cookies (random IDs, used by us to record your donations and avoid repeating ads):
+					<br /> 
+					<div className='word-wrap'>
+						{trackers.map(xid => <>
+							<div>{XId.id(xid)}</div>
+							<br />
+						</>)}
+					</div>
+				</div>
+				<div>
+					Currently logged into Good-Loop via: 
+					<div className='word-wrap'>
+						{authd.map(xid => XId.service(xid)+': '+XId.id(xid)).join(", ")}
+					</div>
+					<br />
+				</div>
+				<div>
+					Links: 
+					{
+						peeps.length 
+							? peeps.map(peep => <>
+								<div key={Person.id(peep)}>
+									{Person.id(peep)} -> {peep.links && peep.links.length? peep.links.map(link => <><div> {link.v} </div><br /></>) : 'no links'}
+								</div>
+								<br />
+								</>)
+							: ' no links found ' 
+
+					}
+				</div>
 			</div>
 		</div>
 	);
