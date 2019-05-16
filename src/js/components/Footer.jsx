7b9b7ac42c5f1ef2132d2952bce7bdc5c929dd71
Mark Wood <markw13a@gmail.com>
2019-05-16 14:52:18 +0100

    Footer

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -3,25 +3,30 @@ import { join } from 'wwutils';
 import { SocialMediaFooterWidget } from './SocialLinksWidget';
 import MDText from '../base/components/MDText';
 
-// usign css grid (and flex in ie10+) to make the footer mobile responsive & had to create innerFooter divs to align content to the bottom using display:table
 const Footer = ({className, leftFooter, rightFooter, style}) => (
 	<div className={join('footer pad1', className)} style={style}>
 		<SocialMediaFooterWidget 
 			type='goodloop'
 			fb_url='https://www.facebook.com/the.good.loop/'
 			tw_url='https://twitter.com/goodloophq'
-			insta_url='https://www.instagram.com/good.loop.ads/'
+			insta_url='https://www.instagram.com/goodloophq/'
 		/>
 		<div className='footer-col leftFooter'>
 			<div className='innerLeftFooter'>
 				<MDText source={leftFooter} />
 			</div>
 		</div>
-		<div className='footer-col mainFooter'>
-			<p>
-				Ads for Good by Good-Loop Ltd.<br />
-				&copy; 2019 Good-Loop <a href="mailto:daniel@good-loop.com?Subject=My-Loop%20query" target="_top">Contact Us</a>
-				&nbsp; This web-app is open-source on <a target='_blank' href='https://github.com/good-loop/my-loop'>GitHub</a>.
+		<div className='mainFooter flex-column'>
+			<p className='text-block'>
+				&copy; 2016-2019 Good-Loop Ltd. <a href=''>Privacy policy</a>
+				<br />
+				Registered in Scotland, Uk (No. SC548356)
+				<br />
+				127 Rose Street South Lane, Edinburgh, EH2 4BB
+				<br />
+			</p>
+			<p className='margin1'>
+				This web-app is open-source on <a target='_blank' href='https://github.com/good-loop/my-loop'>GitHub</a>.
 			</p>
 		</div>
 		<div className='footer-col rightFooter'>
