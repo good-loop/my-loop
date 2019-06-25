937236bc598c00aa2a7fa8796f038781988c464b
Mark Wood <markw13a@gmail.com>
2019-06-25 11:18:00 +0100

    Got rid of placeholder campaign page links

--- src/js/components/cards/RecentCampaignsCard.jsx
+++ src/js/components/cards/RecentCampaignsCard.jsx
@@ -9,45 +9,54 @@ const RecentCampaignsCard = () => {
 					name: "KitKat",
 					adid: "xsINEuJV",
 					logo: "/img/vertiser-logos/kitkat-logo-scaled.png",
-					background: "https://as.good-loop.com/uploads/anon/kitkat__stats1__mini-6306904842484749395.jpg"
+					background: "/img/kitkatpic.jpg",
+					charityName: 'Cocoa Initiative'
 				},
 				{
 					name: "H&M",
 					adid: "CeuNVbtW",
 					logo: "/img/vertiser-logos/HM-logo.png",
-					background: "https://as.good-loop.com/uploads/anon/kitkat__stats1__mini-6306904842484749395.jpg"
+					background: "/img/hmpic.jpg",
+					charityName: 'Water Aid'
 				},
 				{
 					name: "Linda McCartney",
 					adid: "qprjFW1H",
 					logo: "/img/vertiser-logos/linda-mac-logo.png",
-					background: "https://as.good-loop.com/uploads/anon/kitkat__stats1__mini-6306904842484749395.jpg"
+					background: "/img/lindamccartneypic.jpg",
+					charityName: 'Food Banks'
 				},
 				{
 					name: "WWF",
 					adid: "I6g1Ot1b",
 					logo: "/img/vertiser-logos/wwf-logo.png",
-					background: "https://as.good-loop.com/uploads/anon/kitkat__stats1__mini-6306904842484749395.jpg"
+					background: "/img/wwfpic.jpg",
+					charityName: 'WWF'
 				},
 				{
 					name: "Doom Bar",
 					adid: "ma9LU5chyU",
 					logo: "/img/vertiser-logos/doom-bar-logo.png",
-					background: "https://as.good-loop.com/uploads/anon/kitkat__stats1__mini-6306904842484749395.jpg"
+					background: "/img/doombarpic.jpg",
+					charityName: 'Beach Cleans'
 				},
 				{
 					name: "British Gas",
 					adid: "tEfu6NjSY5",
 					logo: "/img/vertiser-logos/bg-logo-mobile.svg",
-					background: "https://as.good-loop.com/uploads/anon/kitkat__stats1__mini-6306904842484749395.jpg"
+					background: "/img/britishgaspic.jpg",
+					charityName: 'Carers UK'
 				}
 			];
 	return (
 		<div className="flex-row flex-wrap">
-			{	vertisers.map(({adid, background, name, logo}) => 
+			{	vertisers.map(({adid, background, charityName, name, logo}) => 
 				<a key={adid} href={"/#campaign/?gl.vert="+adid}>
 					<SquareLogo alt={name} url={background}>
 						<img src={logo} alt='vertiser-logo' />
+						<div className='text-container sub-header white'>
+							<span>{charityName}</span>
+						</div>
 					</SquareLogo>
 				</a>)	
 			}
