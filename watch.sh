3908218f47825ac84a648c484e387ad77c69beaf
Roscoe <roscoe@winterwell.com>
2019-07-22 15:44:44 +0100

    Compilation: Use webpack config and watch script from wwappbase.js/template

--- watch.sh
+++ watch.sh
@@ -1,9 +1,1 @@
-# webpack --display-error-details --watch
-npm i
-if [[ $1 = 'prod' ]]; then
-	echo "Compiling and watching with production optimisation"
-	npm run compile-watch
-else
-	echo "Compiling and watching - dev/debug only"
-	npm run compile-watch-fast
-fi
+../wwappbase.js/template/watch.sh
