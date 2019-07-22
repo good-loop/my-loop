afef7185ec2278af91b5077db9dc8677a75f46be
Roscoe <roscoe@winterwell.com>
2019-07-22 15:34:26 +0100

    Update watch.sh to use new compile-watch-fast npm script

--- watch.sh
+++ watch.sh
@@ -1,3 +1,9 @@
 # webpack --display-error-details --watch
-npm i &
-npm run compile-watch
+npm i
+if [[ $1 = 'prod' ]]; then
+	echo "Compiling and watching with production optimisation"
+	npm run compile-watch
+else
+	echo "Compiling and watching - dev/debug only"
+	npm run compile-watch-fast
+fi
