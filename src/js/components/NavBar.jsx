2fc64e33196fb09112437b260256e26beb7cb5ac
Roscoe <roscoe@winterwell.com>
2019-08-14 13:29:51 +0100

    Navbar was attempting to reference non-existent "props" when not logged in

--- src/js/components/NavBar.jsx
+++ src/js/components/NavBar.jsx
@@ -4,6 +4,7 @@ import Login from 'you-again';
 import NavBar from '../base/components/NavBar';
 import C from '../C';
 import {navBarLogoContainerSVG} from './svg';
+import {LoginLink} from '../base/components/LoginWidget';
 
 
 const MyLoopNavBar = props => (
@@ -11,46 +12,38 @@ const MyLoopNavBar = props => (
 		<NavBar
 			{...props}
 			pages={[]}
-			render={({pageLinks, currentPage, style, logo}) => (
-				<>
-					{navBarLogoContainerSVG}
-					<div style={style}>
-						<div className="navbar-header" title="Dashboard">
-							<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-								<span className="sr-only">Toggle navigation</span>
-								<span className="icon-bar" /><span className="icon-bar" /><span className="icon-bar" />
-							</button>
-							<a className="navbar-brand" href='/'>
-								<img alt={C.app.name} src={logo || C.app.homeLogo || C.app.logo} />
-							</a>
-						</div>
-						<div id="navbar" className="navbar-collapse collapse">
-							<ul className="nav navbar-nav">
-								{pageLinks}
-							</ul>
-							<div>
-								<AccountMenu active={currentPage === 'account'} logoutLink='#my' />
-							</div>
+			render={({pageLinks, currentPage, style, logo}) => <>
+				{navBarLogoContainerSVG}
+				<div style={style}>
+					<div className="navbar-header" title="Dashboard">
+						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span className="sr-only">Toggle navigation</span>
+							<span className="icon-bar" /><span className="icon-bar" /><span className="icon-bar" />
+						</button>
+						<a className="navbar-brand" href='/'>
+							<img alt={C.app.name} src={logo || C.app.homeLogo || C.app.logo} />
+						</a>
+					</div>
+					<div id="navbar" className="navbar-collapse collapse">
+						<ul className="nav navbar-nav">
+							{pageLinks}
+						</ul>
+						<div>
+							<AccountMenu active={currentPage === 'account'} logoutLink='#my' />
 						</div>
 					</div>
-				</>
-			)}
+				</div>
+			</>}
 		/>
 	</div>
 );
 
 const AccountMenu = ({active, logoutLink}) => {
-	const isMobile = window.innerWidth <= 767;
-	
-	// TODO see navbar dropdown
-	if ( ! Login.isLoggedIn()) {
-		return (
-			<ul id='top-right-menu' className="nav navbar-nav navbar-right">
-				{props.noRegister ? '' : <li><RegisterLink /></li>}
-				<li><LoginLink /></li>
-			</ul>
-		);
-	}
+	if (!Login.isLoggedIn()) return (
+		<ul id='top-right-menu' className="nav navbar-nav navbar-right">
+			<li><LoginLink /></li>
+		</ul>
+	);
 
 	let user = Login.getUser();
 
