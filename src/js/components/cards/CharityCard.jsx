89a230b27e94bc66937e505bda126d05833f29b6
DW-Stross <daniel@good-loop.com>
2019-10-15 10:54:50 +0100

    cards-refactor-wip2

---
+++ src/js/components/cards/CharityCard.jsx
@@ -0,0 +1,63 @@
+
+import React from 'react';
+import ACard from './ACard';
+import Roles from '../../base/Roles';
+import C from '../../C';
+import DataStore from '../../base/plumbing/DataStore';
+import Misc from '../../base/components/Misc';
+import ActionMan from '../../plumbing/ActionMan';
+import NGO from '../../base/data/NGO';
+import { SquareLogo } from '../Image';
+import ReactMarkdown from 'react-markdown';
+
+const CharityCard = ({charity}) => {
+	// fetch extra info from SoGive
+	let cid = charity.id;
+	if (cid) {
+		const pvCharity = ActionMan.getDataItem({type:C.TYPES.NGO, id:charity.id, status:C.KStatus.PUBLISHED});
+		let sogiveCharity = pvCharity.value;
+		if (sogiveCharity) {		
+			// HACK: prefer short description
+			if (sogiveCharity.summaryDescription) sogiveCharity.description = sogiveCharity.summaryDescription;
+			// merge in SoGive as defaults
+			charity = Object.assign({}, sogiveCharity, charity);
+			cid = NGO.id(sogiveCharity); // see ServerIO's hacks to handle bad data entry in the Portal
+		}
+	}
+
+	// If charity has photo, use it. Otherwise use logo with custom colour bg and eliminate name.
+	let photo = charity.highResPhoto || charity.images;
+	let logo = charity.logo;
+
+	let backgroundColor = charity.color;
+
+	return (<ACard backgroundColor={backgroundColor} name={cid} >
+		<div className='charity-card' key={charity.name}>
+			<a className='flex-row charity' href={charity.url} target="_blank" rel="noopener noreferrer"
+				style={photo || !charity.color ? {} : {background: charity.color}}
+			>
+				{photo && logo? <img className='logo-small' src={logo} style={{position:"relative",top:0,left:0}} /> : null}
+
+				<SquareLogo url={photo || logo} className={photo? 'contain' : null} />
+				<span className='name sub-header pad1 white contrast-text'>
+					{photo ? charity.name : ''}
+				</span>
+			</a>
+			<CharityCard2 charity={charity} />
+			<div>TODO Impact, or £s donated</div>
+			<div>TODO thank-you comment from the charity, if we received one</div>
+			{Roles.isDev() && cid? <small><a href={'https://app.sogive.org/#simpleedit?charityId='+escape(cid)} target='_sogive'>SoGive</a></small> : null}
+		</div>
+	</ACard>);
+};
+
+const CharityCard2 = ({charity}) => {
+	// impact data?? e.g. you funded 10 trees <-- This would be best when we can TODO
+	if (charity.description) {
+		return <div className='charity-description text-block'><ReactMarkdown source={charity.description} /></div>;
+	}
+	// TODO money donated to this charity??
+	return null;
+};
+
+export default CharityCard;
