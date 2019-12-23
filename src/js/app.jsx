0bd877a2207f67da158aa575fd056cc90fbd670f
aundiks@gmail.com <andris@good-loop.com>
2019-12-23 15:09:45 +0000

    MyPage: Fixed navbar sticky problem. Fixed classnames generated for FA5 icons.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Misc from './base/components/Misc';
 // import 'bootstrap/dist/css/bootstrap.min.css'; TODO inline BS -- maybe into BS3.jsx and BS4.jsx??
 import SJTest, {assert} from 'sjtest';
 
 import MainDiv from './components/MainDiv';
+
+// Pass font awesome version onto Misc, so it adds the appropiate class to the icons
+Misc.FontAwesome = 5;
 
 ReactDOM.render(
 	<MainDiv />,
