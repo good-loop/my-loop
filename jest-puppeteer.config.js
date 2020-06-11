7b334d057f1b7f3b53411cfd3d5675547191e2d9
DA <daniel.appel.winterwell@gmail.com>
2020-06-11 16:50:43 +0100

    removing individual non-canonical jest-puppeteer.config.js and runtest.js symlink

--- jest-puppeteer.config.js
+++
@@ -1,9 +0,0 @@
-const config = JSON.parse(process.env.__CONFIGURATION);
-console.log(config)
-
-module.exports = {
-	launch: {
-		headless: config.head,
-		executablePath: config.chrome ? '/usr/bin/google-chrome-stable' : ''
-	}
-};
