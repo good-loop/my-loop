4cc341881a8ad2acac779dc4fed3d1c82e955ef3
Daniel Winterstein <daniel@good-loop.com>
2020-06-27 09:05:51 +0100

    css-build-update

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -6,10 +6,6 @@ import SJTest, {assert} from 'sjtest';
 import Misc from './base/components/Misc';
 import MainDiv from './components/MainDiv';
 
-// Import root LESS file so webpack finds & renders it out to main.css
-import '../style/main.less';
-// import 'bootstrap/dist/css/bootstrap.min.css'; TODO inline BS -- maybe into BS3.jsx and BS4.jsx??
-
 window.$ = $; // HACK needed for youagain
 
 // Pass font awesome version onto Misc, so it adds the appropiate class to the icons
