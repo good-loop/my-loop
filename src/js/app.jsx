5e25eae721407fb797753548d00b51e8b5266e88
Daniel Winterstein <daniel@good-loop.com>
2020-06-27 18:18:57 +0100

    sync

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import SJTest, {assert} from 'sjtest';
+import SJTest from 'sjtest';
 
 import Misc from './base/components/Misc';
 import MainDiv from './components/MainDiv';
 
-window.$ = $; // HACK needed for youagain
+// Import root LESS file so webpack finds & renders it out to main.css
+import '../style/main.less';
 
 // Pass font awesome version onto Misc, so it adds the appropiate class to the icons
 Misc.FontAwesome = 5;
+// global jquery for You-Again
+window.$ = $;
 
 ReactDOM.render(
 	<MainDiv />,
