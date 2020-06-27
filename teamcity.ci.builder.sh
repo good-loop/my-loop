cd1386db6f4aef05f959e17ba47e2f1e5c52e6e9
Daniel Winterstein <daniel@good-loop.com>
2020-06-27 09:03:34 +0100

    update-CI

--- teamcity.ci.builder.sh
+++ teamcity.ci.builder.sh
@@ -2,7 +2,7 @@
 
 # TeamCity Continuous Integration Builder Template
 
-#Version 1.0
+#Version 1.1
 # Meaning - Script has been written and tested
 
 #####  GENERAL SETTINGS
@@ -45,18 +45,21 @@ BOB_ARGS='' #you can set bob arguments here, but they will run each and every ti
 BOB_BUILD_PROJECT_NAME='' #If the project name isn't automatically sensed by bob, you can set it explicitly here
 NPM_CLEANOUT='no' #yes/no , will nuke the node_modules directory if 'yes', and then get brand-new packages.
 
+NPM_I_LOGFILE="/home/winterwell/.npm/_logs/npm.i.for.$PROJECT_NAME.log"
+NPM_RUN_COMPILE_LOGFILE="/home/winterwell/.npm/_logs/npm.run.compile.for.$PROJECT_NAME.log"
 
 ##### FUNCTIONS
 ## Do not edit these unless you know what you are doing
 #####
 
+ATTACHMENTS=()
 function send_alert_email {
     for email in ${EMAIL_RECIPIENTS[@]}; do
         TIME=$(date +%Y-%m-%dT%H:%M:%S-%Z)
-	    message="AutoBuild Detected a Failure with $BUILD_PROCESS_NAME"
+	    message="AutoBuild Detected a Failure Building $PROJECT_NAME during $BUILD_PROCESS_NAME"
 	    body="Hi,\nThe AutoPublisher detected a failure when $BUILD_STEP"
-	    title="[$HOSTNAME] $message"
-	    printf "$body" | mutt -s "$title" -- $email
+	    title="TeamCity $message"
+	    printf "$body" | mutt -s "$title" ${ATTACHMENTS[@]} -- $email
     done
 }
 
@@ -179,15 +182,21 @@ function use_npm {
                 printf "\nDeleting the existing node_modules...\n"
                 ssh winterwell@$server "rm -rf $PROJECT_ROOT_ON_SERVER/node_modules"
             fi
+            # Ensuring that there are no residual npm error/debug logs in place
+            ssh winterwell@$server "rm -rf /home/winterwell/.npm/_logs/*.log"
             printf "\nEnsuring all NPM Packages are in place on $server ...\n"
-            ssh winterwell@$server "cd $PROJECT_ROOT_ON_SERVER && npm i"
+            ssh winterwell@$server "cd $PROJECT_ROOT_ON_SERVER && npm i &> $NPM_I_LOGFILE"
             printf "\nChecking for errors while npm was attempting to get packages on $server ...\n"
-            if [[ $(ssh winterwell@$server "grep -i 'error' /home/winterwell/.npm/_logs/$NPM_LOG_DATE*-debug.log") = '' ]]; then
+            if [[ $(ssh winterwell@$server "grep -i 'error' $NPM_I_LOGFILE") = '' ]]; then
                 printf "\nNo NPM errors detected\n"
             else
-                printf "\nNPM encountered one or more errors while attempting to get node packages. Sending Alert Emails, and Breaking Operation\n"
+                printf "\nNPM encountered one or more errors while attempting to get node packages. Sending Alert Emails, but Continuing Operation\n"
+                # Get the NPM_I_LOGFILE
+                scp winterwell@$server:$NPM_I_LOGFILE .
+                # Add it to the Attachments
+                ATTACHMENTS+=("-a npm.i.for.$PROJECT_NAME.log")
+                # Send the email
                 send_alert_email
-                exit 0
             fi
         done
     fi
@@ -201,14 +210,18 @@ function use_webpack {
         NPM_LOG_DATE=$(date +%Y-%m-%d)
         for server in ${TARGET_SERVERS[@]}; do
             printf "\nNPM is now running a Webpack process on $server\n"
-            ssh winterwell@$server "cd $PROJECT_ROOT_ON_SERVER && npm run compile"
+            ssh winterwell@$server "cd $PROJECT_ROOT_ON_SERVER && npm run compile &> $NPM_RUN_COMPILE_LOGFILE"
             printf "\nChecking for errors that occurred during Webpacking process on $server ...\n"
-            if [[ $(ssh winterwell@$server "grep -i 'error' /home/winterwell/.npm/_logs/$NPM_LOG_DATE*-debug.log") = '' ]]; then
+            if [[ $(ssh winterwell@$server "cat $NPM_RUN_COMPILE_LOGFILE | grep -i 'error' | grep -iv 'ErrorAlert.jsx'") = '' ]]; then
                 printf "\nNo Webpacking errors detected on $server\n"
             else
-                printf "\nOne or more errors were recorded during the webpacking process. Sending Alert Emails, and Breaking Operation\n"
+                printf "\nOne or more errors were recorded during the webpacking process. Sending Alert Emails, but Continuing Operation\n"
+                # Get the NPM_RUN_COMPILE_LOGFILE
+                scp winterwell@$server:$NPM_RUN_COMPILE_LOGFILE .
+                # Add it to the Attachments
+                ATTACHMENTS+=("-a npm.run.compile.for.$PROJECT_NAME.log")
+                # Send the email
                 send_alert_email
-                exit 0
             fi
         done
     fi
