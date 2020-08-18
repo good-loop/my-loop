365b3e6bb70082e06df6a03a70e757372465dc97
BDurkin <ben@good-loop.com>
2020-08-18 17:06:46 +0100

    Chaqrity quotes

--- src/js/components/campaignpage/CharityQuote.jsx
+++ src/js/components/campaignpage/CharityQuote.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Counter from '../../base/components/Counter';
 
 /**
  * HACK hardcode some thank you messages.
@@ -7,33 +8,47 @@ import React from 'react';
  */
 const tq = charity => {
 	return {
-		helenbamber: `"That is absolutely fantastic news, thank you so much! Congratulations everyone on a successful Spring/Summer Campaign! 
-		The donation will go a huge way in supporting our clients to recover and rebuild their lives." -- Sophie at Helen Bamber`,
+		helenbamber: {quote:`"That is absolutely fantastic news, thank you so much! Congratulations everyone on a successful Spring/Summer Campaign! 
+		The donation will go a huge way in supporting our clients to recover and rebuild their lives."`,
+					source: "Sophie at Helen Bamber"},
 
 		// TODO name
-		wwf: `"The money raised through the H&M campaign will support WWF UK's vital work, fighting for a world where people and nature can
+		wwf: {
+			img: "/img/WWF_FeelGoodImage.png",
+			quote:`"The money raised through the H&M campaign will support WWF UK's vital work, fighting for a world where people and nature can
 thrive, and continue to support schools, teachers and pupils to
 develop their knowledge and understanding of the environmental
-challenges facing our planet." -- Chiara Cadei, WWF`
+challenges facing our planet."`,
+			source: "Chiara Cadei, WWF"}
 	}[charity.id] || "";
 };
 
-const CharityQuote = ({charity}) => {
+const CharityQuote = ({charity, donationValue}) => {
 
 	let quote = tq(charity);
-
     return (
-        quote ?
-			<div className="charity-quote">
-				<div className="charity-quote-img">
-
-				</div>
-				<div className="charity-quote-text">
-					{quote}
+		quote ?
+			<div className="p-3">
+				<div className="charity-quote row">
+					<div className="charity-quote-img col-md-5 p-0">
+						<img src={quote.img} className="w-100"/>
+					</div>
+					<div className="charity-quote-content col-md-7">
+						<div className="charity-quote-logo">
+							<img src={charity.logo}/>
+						</div>
+						<div className="charity-quote-text">
+							{donationValue ? <h2><Counter currencySymbol='&pound;' value={donationValue} /> raised</h2> : null}
+							<p class="font-italic">
+								{quote.quote}
+							</p>
+							<p>{quote.source}</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		: null
     );
 }
 
-export default CharityQuote;
+export {CharityQuote, tq}
