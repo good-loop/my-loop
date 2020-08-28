f515c688f485a994f62b87f942ae092fcdd2c581
Daniel Winterstein <daniel@good-loop.com>
2020-08-28 09:21:46 +0100

    BannerAd

---
+++ src/js/components/BannerAd.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const BannerAd = () => {	
+	//<!-- BEGIN JS TAG - banner placement for newtab (desktop) < - DO NOT MODIFY -->
+	// https://console.appnexus.com/placement?id=1610003
+	return (<script src="http://ib.adnxs.com/ttj?id=17741445&size=[WIDTHxHEIGHT]" type="text/javascript"></script>);
+};
+
+export default BannerAd;
