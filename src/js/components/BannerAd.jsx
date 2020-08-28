cf6aa153bbcbcf2a06ddd5766a6dc6a4a4c55d2f
Daniel Winterstein <daniel@good-loop.com>
2020-08-28 09:48:49 +0100

    ad-in-html

--- src/js/components/BannerAd.jsx
+++ src/js/components/BannerAd.jsx
@@ -1,10 +1,15 @@
 
 import React from 'react';
+import DataStore from '../base/plumbing/DataStore';
+import { addScript } from '../base/utils/miscutils';
 
 const BannerAd = () => {	
 	//<!-- BEGIN JS TAG - banner placement for newtab (desktop) < - DO NOT MODIFY -->
 	// https://console.appnexus.com/placement?id=1610003
-	return (<script src="http://ib.adnxs.com/ttj?id=17741445&size=[WIDTHxHEIGHT]" type="text/javascript"></script>);
+	let placementId = DataStore.getUrlValue("placement") || 20079232;
+	// addScript({}, domElement);
+	return "TODO dynamic script tag";
+	// return (<script src="http://ib.adnxs.com/ttj?id={placementId}&size=[WIDTHxHEIGHT]" type="text/javascript"></script>);
 };
 
 export default BannerAd;
