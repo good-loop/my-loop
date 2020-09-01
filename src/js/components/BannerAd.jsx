f5944bbf807ce69fdf0a723a46c7b5cb4028b521
Daniel Winterstein <daniel@good-loop.com>
2020-09-01 14:05:31 +0100

    dynamic-script

--- src/js/components/BannerAd.jsx
+++ src/js/components/BannerAd.jsx
@@ -7,20 +7,21 @@ const BannerAd = () => {
 	//<!-- BEGIN JS TAG - banner placement for newtab (desktop) < - DO NOT MODIFY -->
 	// https://console.appnexus.com/placement?id=1610003
 	let placementId = DataStore.getUrlValue("placement") || 20079232;
-	return <Script src={`http://ib.adnxs.com/ttj?id=${placementId}&size=300x250`} />;
+	return <div>BannerAd here <Script src={`http://ib.adnxs.com/ttj?id=${placementId}&size=300x250`} /></div>;
 	// addScript({}, domElement);
 	// return (<script src="http://ib.adnxs.com/ttj?id={placementId}&size=[WIDTHxHEIGHT]" type="text/javascript"></script>);
 };
 
 const scripts = {};
 
-const Script = ({src}) => {
+const Script = ({src, async, onload, onerror}) => {
 	// const ref = useRef();
 	if (scripts[src]) {
-		return;
+		return null;
 	}
-	addScript({src}); // async, onload, onerror, domElement
+	addScript({src, async, onload, onerror}); // async, onload, onerror, domElement
 	scripts[src] = true;
+	return null;
 };
 
 export default BannerAd;
