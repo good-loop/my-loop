408539d50e5aecb75ec437149fb37e29ca91ca51
BDurkin <ben@good-loop.com>
2020-09-04 13:15:17 +0100

    Changed footer link

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = ({className, leftFooter, rightFooter, style, showSocialMediaLinks
 		</div>
 		<div className='mainFooter flex-column p-3'>
 			<p>
-				&copy; 2016-2019 <a href='https://www.good-loop.com/blog'>Good-Loop Ltd</a>, all rights reserved.<br />
+				&copy; 2016-2019 <a href='https://good-loop.com/good-news/index'>Good-Loop Ltd</a>, all rights reserved.<br />
 				<a href='https://www.good-loop.com/privacy-policy'>Privacy policy</a>
 			</p>
 			<p>
