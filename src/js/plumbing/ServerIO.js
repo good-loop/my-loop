9cba5a6fee99757500534a343f1d635eec5dc25e
Daniel Winterstein <daniel@good-loop.com>
2020-11-26 22:07:22 +0000

    consent settings work

--- src/js/plumbing/ServerIO.js
+++ src/js/plumbing/ServerIO.js
@@ -31,7 +31,8 @@ ServerIO.DATALOG_ENDPOINT = 'https://lg.good-loop.com/data';
 
 ServerIO.PROFILER_ENDPOINT = `${C.HTTPS}://${C.SERVER_TYPE}profiler.good-loop.com`;
 // ServerIO.PROFILER_ENDPOINT = 'https://testprofiler.good-loop.com';
-ServerIO.PROFILER_ENDPOINT = 'https://profiler.good-loop.com';
+ServerIO.PROFILER_ENDPOINT = 'http://localprofiler.good-loop.com';
+// ServerIO.PROFILER_ENDPOINT = 'https://profiler.good-loop.com';
 
 // ServerIO.AS_ENDPOINT = `${C.HTTPS}://${C.SERVER_TYPE}as.good-loop.com`;
 // ServerIO.AS_ENDPOINT = `https://testas.good-loop.com`;
