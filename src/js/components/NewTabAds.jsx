4ee242d100f9afb655d34ad4e09be64e6489dbeb
dodo721 <dodoworkspr@gmail.com>
2021-02-02 12:41:44 +0000

    Ad updates

--- src/js/components/NewTabAds.jsx
+++ src/js/components/NewTabAds.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NewTabAds = () => {
     return (
-        <div id="tab-ads" class="position-absolute w-100 flex-row" style="bottom:0; height: 115px; padding: 10px">
+        <div id="tab-ads" className="position-absolute w-100 flex-row" style={{bottom:0, height: 115, padding: 10}}>
             <script type="text/javascript">
                 // HACK - ads wont serve in iframe because it sees empty content before webpack is loaded.
                 // This tricks AdSense into monitoring the parent instead, which it is happy with
@@ -12,7 +12,7 @@ const NewTabAds = () => {
             </script>
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             {/* Tabs for Good bottom display */}
-            <ins class="adsbygoogle"
+            <ins className="adsbygoogle"
                 style="display:inline-block;width:728px;height:90px"
                 data-ad-client="ca-pub-9448086783562205"
                 data-ad-slot="5736021145"></ins>
