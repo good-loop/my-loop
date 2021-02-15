833125995695e8c7481578dbf2f7b09520aa1e8e
dodo721 <dodoworkspr@gmail.com>
2021-02-15 15:45:27 +0000

    Open chrome store in new tab

--- src/js/components/pages/NewtabCharityLogin.jsx
+++ src/js/components/pages/NewtabCharityLogin.jsx
@@ -148,6 +148,7 @@ const LogInForm = ({onRegister, onLogin}) => {
 		<p>Step 1 complete!<br/>
 		Now install the plugin for Chrome.</p>
 		<a className="btn btn-primary mt-2"
+			target="_blank"
 			href="https://chrome.google.com/webstore/detail/good-loop-tabs-for-good/baifmdlpgkohekdoilaphabcbpnacgcm?hl=en&authuser=1">
 			CHROME STORE
 		</a>
