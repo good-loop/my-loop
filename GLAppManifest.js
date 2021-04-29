602f2a61449be69fd84222576f89f6b759be144a
Daniel Winterstein <daniel@good-loop.com>
2021-04-29 22:45:39 +0100

    tweak

--- GLAppManifest.js
+++ GLAppManifest.js
@@ -1,12 +1,12 @@
 /**
- * 
+ * My.Good-Loop
  */
 const GLAppManifest = {
 	name: "My.Good-Loop",
 	id: "my.good-loop.com",
 	dataspace: "good-loop",
 	logo: "/img/gl-logo/LogoMark/logo.svg",
-	website: "https://good-loop.com",
+	website: "https://good-loop.com",	
 	facebookAppId: "320927325010346", // https://developers.facebook.com/apps/320927325010346/dashboard/
 	privacyPolicy: "https://doc.good-loop.com/privacy-policy.html",
 	tsncs: "https://doc.good-loop.com/terms/terms-of-use.html"
