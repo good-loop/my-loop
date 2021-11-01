ceb448854a0819db7adfcab5d69b453d73acaac9
Wing Wong <wing@good-loop.com>
2021-11-01 14:14:14 +0000

    whitelist filenames to allowlist

--- src/js/components/pages/AllowlistUs.jsx
+++ src/js/components/pages/AllowlistUs.jsx
@@ -12,8 +12,8 @@ window.onload = () => {
 const AllowlistUs = () => {
 	return (<>
 		<MyLoopNavBar logo="/img/new-logo-with-text.svg" logoScroll="/img/new-logo-with-text-white.svg" />
-		<img className="adblock-arrow" src="/img/whitelist/adblock-arrow.png" alt="" />
-		<div className="WhitelistUs">
+		<img className="adblock-arrow" src="/img/allowlist/adblock-arrow.png" alt="" />
+		<div className="AllowlistUs">
 			<div className="m-5">
 				<br/> <br/>
 			</div>
@@ -27,11 +27,11 @@ const AllowlistUs = () => {
 				<Row className="text-center text-md-left">
 					<Col md={6}>
 						<div className="justify-content-left">
-							<p>We can't raise money for charity without displaying ads. Please disable your adblocker or whitelist us so Tabs for Good can work!</p>
+							<p>We can't raise money for charity without displaying ads. Please disable your adblocker or allowlist us so Tabs for Good can work!</p>
 						</div>
 					</Col>
 					<Col md={6}>
-						<img className="w-100" src="/img/whitelist/red-toggle-on.png"/>
+						<img className="w-100" src="/img/allowlist/red-toggle-on.png"/>
 					</Col>
 				</Row>
 				<div className="m-5">
@@ -39,16 +39,16 @@ const AllowlistUs = () => {
 				</div>
 				<Row className="text-center text-md-right justify-content-end">
 					<Col md={8}>
-						<h1 className="ml-auto">Whitelist Tabs For Good.</h1>
+						<h1 className="ml-auto">Allowlist Tabs For Good.</h1>
 					</Col>
 				</Row>
 				<Row className="text-center text-md-right">
 					<Col md={6} className="text-md-center">
-						<img className="w-100" src="/img/whitelist/turnoffadblockerflowery.png"/>
+						<img className="w-100" src="/img/allowlist/turnoffadblockerflowery.png"/>
 					</Col>
 					<Col md={6}>
 						<div className="justify-content-center">
-							<p>Simply pause Adblocker on this page or manually add <br/> <code>my.good-loop.com</code> to your ad blocker whitelist filter to <br/> enable us to start raising money for charity.</p>
+							<p>Simply pause Adblocker on this page or manually add <br/> <code>my.good-loop.com</code> to your ad blocker allowlist/ allowlist filter to <br/> enable us to start raising money for charity.</p>
 						</div>
 					</Col>
 				</Row>
