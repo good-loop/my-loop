d6a06cb6396f78ab1cda09724fe9565a2bc57c09
Daniel Winterstein <daniel@good-loop.com>
2022-01-19 23:04:04 +0000

    bob build

---
+++ builder/com/goodloop/myloop/BuildMyLoop.java
@@ -0,0 +1,15 @@
+package com.goodloop.myloop;
+
+import java.io.File;
+
+import com.winterwell.bob.wwjobs.BuildWinterwellProject;
+
+public class BuildMyLoop extends BuildWinterwellProject {
+
+	public BuildMyLoop() {
+		super("my-loop");
+		setVersion("0.1.0"); // Jan 2022
+		setMainClass("com.goodloop.myloop.MyLoopMain");
+	}
+	
+}
