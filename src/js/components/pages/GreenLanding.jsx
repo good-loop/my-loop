0a194453a7a5d701d1fc6e33c047093be4e5a11c
Daniel Winterstein <daniel@good-loop.com>
2022-01-23 16:28:16 +0000

    sync

--- src/js/components/pages/GreenLanding.jsx
+++ src/js/components/pages/GreenLanding.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
 
-const GreenLanding = ({}) => {
-  return <div id="green-landing">
-    <div className="green-landing-splash">
-      BRAND LOGO HERE<br/>
-      <div className="splash-tonnes">XXXX TONNES</div>
-      carbon offset
-      <div className="splash-trees">XXXX</div>
-      trees planted<br/>
-      with
-      <div className="splash-carbon-neutral">
-        CARBON NEUTRAL ADS<br/>
-        BY GOOD-LOOP
-      </div>
-      <a className="splash-explore">EXPLORE OUR IMPACT</a>
-    </div>
-  </div>;
+const GreenLanding = ({ }) => {
+	// like CampaignPage, this would prefer to run by a campaign id
+	
+	return <div id="green-landing">
+		<div className="green-landing-splash">
+			BRAND LOGO HERE<br />
+			<div className="splash-tonnes">XXXX TONNES</div>
+			carbon offset
+			<div className="splash-trees">XXXX</div>
+			trees planted<br />
+			with
+			<div className="splash-carbon-neutral">
+				CARBON NEUTRAL ADS<br />
+				BY GOOD-LOOP
+			</div>
+			<a className="splash-explore">EXPLORE OUR IMPACT</a>
+		</div>
+	</div>;
 };
 
 export default GreenLanding;
