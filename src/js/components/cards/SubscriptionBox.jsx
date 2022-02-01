e49f8ff7e8594ac1c2f139bd3a66625cd495a8c8
Daniel Winterstein <daniel@good-loop.com>
2022-02-01 20:01:23 +0000

    account-menu-alignment

--- src/js/components/cards/SubscriptionBox.jsx
+++ src/js/components/cards/SubscriptionBox.jsx
@@ -44,7 +44,7 @@ const SubscriptionBox = ({className="", title="Support the causes you care about
 						/>
 					</FormGroup>
 					<div className="mobile-break"/>
-					<Button color="primary" disabled={hasSubmittedEmail} className="flex-grow-0">
+					<Button color="secondary" disabled={hasSubmittedEmail} className="flex-grow-0">
 						Sign me up
 					</Button>
 				</Form>
