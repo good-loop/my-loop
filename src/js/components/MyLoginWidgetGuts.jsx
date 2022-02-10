d5deb3d5f56e392d57f035d2958d968df62f3938
dodo721 <dodoworkspr@gmail.com>
2022-02-10 16:22:16 +0000

    Login form styling

--- src/js/components/MyLoginWidgetGuts.jsx
+++ src/js/components/MyLoginWidgetGuts.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { EmailSignin } from '../base/components/LoginWidget';
+import { Row, Col, Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
+import { EmailSignin, setShowLogin, VERB_PATH } from '../base/components/LoginWidget';
+import DataStore from '../base/plumbing/DataStore';
+import { T4GSignUpLink } from './T4GSignUp';
 
 export const MyLoginWidgetGuts = ({services, verb, onLogin, onRegister, noRegister}) => {
 	if (!verb) verb = DataStore.getValue(VERB_PATH) || 'login';
 	return (
 		<div className="login-guts container-fluid">
-			<div className='d-flex flex-column justify-content-center align-items-center'>
+			<div className='d-flex flex-column justify-content-center align-items-center p-5'>
 				<img src="/img/gl-logo/TabsForGood/TabsForGood_Logo-01.png" className='login-logo'/>
-				<h4 className='black login-subtitle'>Sign in to see how your web browsing has transformed into charity donations</h4>
-				<Row>
+				<p className='leader-text black login-subtitle my-4'>Sign in to see how your web browsing has transformed into charity donations</p>
+				<Row className='w-50'>
 					<Col className="login-email pb-2">
 						<EmailSignin
 							verb={verb}
 							onLogin={onLogin}
 							onRegister={onRegister}
 							noRegister={noRegister}
+							className="myloop-email-login"
 						/>
 					</Col>
 					{/* Removed for now - no design for social services
@@ -23,6 +27,7 @@ export const MyLoginWidgetGuts = ({services, verb, onLogin, onRegister, noRegist
 					</Col>
 					*/}
 				</Row>
+				<T4GSignUpLink onClick={() => setShowLogin(false)} className="mt-5">Not got an account? Sign up and get Tabs for Good</T4GSignUpLink>
 			</div>
 		</div>
 	);
