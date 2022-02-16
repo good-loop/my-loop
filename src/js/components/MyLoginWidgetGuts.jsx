cbec69a54897c5deabcf57ea6d85133bcc6ad093
Wing Wong <me@nikowong.com>
2022-02-16 17:14:15 +0000

    mobile login row > CSS

--- src/js/components/MyLoginWidgetGuts.jsx
+++ src/js/components/MyLoginWidgetGuts.jsx
@@ -8,7 +8,7 @@ import { T4GSignUpLink } from './T4GSignUp';
 export const MyLoginWidgetGuts = ({services, verb, onLogin, onRegister, canRegister}) => {
 	if (!verb) verb = DataStore.getValue(VERB_PATH) || 'login';
 
-	let rowClass = isMobile == true ? "w-100" : 'w-50'
+	// let rowClass = isMobile == true ? "w-100" : 'w-50'
 	return (
 		<div className="login-guts container-fluid position-relative">
 			<div className='d-flex flex-column justify-content-center align-items-center p-5'>
@@ -16,7 +16,7 @@ export const MyLoginWidgetGuts = ({services, verb, onLogin, onRegister, canRegis
 				<img src="/img/signup/hand-globe-coins.png" className='hand-globe d-none d-md-block'/>
 				<img src="/img/gl-logo/TabsForGood/TabsForGood_Logo-01.png" className='login-logo'/>
 				<p className='leader-text black login-subtitle my-4'>Sign in to see how your web browsing has transformed into charity donations</p>
-				<Row className={rowClass}>
+				<Row className="login-row">
 					<Col className="login-email pb-2">
 						<EmailSignin
 							verb={verb}
