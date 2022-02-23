23df5c9d487e6ddbfd0821db5d14712ad05557a1
dodo721 <dodoworkspr@gmail.com>
2022-02-23 12:03:30 +0000

    Smaller testimonial content

--- src/js/components/pages/CommonComponents.jsx
+++ src/js/components/pages/CommonComponents.jsx
@@ -373,39 +373,39 @@ const TestimonialSectionLower = () => {
 				<div className="testimonial-impact text-center">
 					<div className="row pt-5">
 						<div className="col-md-4 testimonial-points row">	
-							<Circle className="mx-auto d-flex d-md-none" width='8em'>
-									<img className='logo logo-lg' src="/img/charity-logos/we-forest.jpeg" alt="We Forest logo" />
+							<Circle className="mx-auto d-flex d-md-none" width='4em'>
+									<img className='logo' src="/img/charity-logos/we-forest.jpeg" alt="We Forest logo" />
 								</Circle>			
 							<div className="col">
-								<Circle className="mx-auto d-md-flex d-none" width='8em'>
-									<img className='logo logo-lg' src="/img/charity-logos/we-forest.jpeg" alt="We Forest logo" />
+								<Circle className="mx-auto d-md-flex d-none" width='4em'>
+									<img className='logo' src="/img/charity-logos/we-forest.jpeg" alt="We Forest logo" />
 								</Circle>
 								<h3 className='pt-md-4'>721.3 Hectares</h3>
-								<p className='leader-text text-uppercase nomargin'>of forest restored - that's 1,000+ football pitches!</p>
+								<p className='nomargin'>of forest restored<br/>(that's 1,000+ football pitches!)</p>
 							</div>
 						</div>
 						<div className="col-md-4 testimonial-points">
-								<Circle className="mx-auto d-flex d-md-none" width='8em'>
-									<img className='logo logo-lg' src="/img/charity-logos/no-kid-hungry.png" alt="No Kid Hungry logo" />
+								<Circle className="mx-auto d-flex d-md-none" width='4em'>
+									<img className='logo' src="/img/charity-logos/no-kid-hungry.png" alt="No Kid Hungry logo" />
 								</Circle>
 							<div className="col">
-								<Circle className="mx-auto d-md-flex d-none" width='8em'>
-									<img className='logo logo-lg' src="/img/charity-logos/no-kid-hungry.png" alt="No Kid Hungry logo" />
+								<Circle className="mx-auto d-md-flex d-none" width='4em'>
+									<img className='logo' src="/img/charity-logos/no-kid-hungry.png" alt="No Kid Hungry logo" />
 								</Circle>
 								<h3 className='pt-md-4'>183,318 Meals</h3>
-								<p className='leader-text text-uppercase nomargin'>provided for children</p>
+								<p className='nomargin'>provided for children</p>
 							</div>
 						</div>
 						<div className="col-md-4 row testimonial-points">
-							<Circle className="mx-auto d-flex d-md-none" width='8em'>
-								<img className='logo logo-lg' src="/img/charity-logos/mind.png" alt="Mind logo" />
+							<Circle className="mx-auto d-flex d-md-none" width='4em'>
+								<img className='logo' src="/img/charity-logos/mind.png" alt="Mind logo" />
 							</Circle>
 							<div className="col">
-								<Circle className="mx-auto d-md-flex d-none" width='8em'>
-									<img className='logo logo-lg' src="/img/charity-logos/mind.png" alt="Mind logo" />
+								<Circle className="mx-auto d-md-flex d-none" width='4em'>
+									<img className='logo' src="/img/charity-logos/mind.png" alt="Mind logo" />
 								</Circle>
 								<h3 className='pt-md-4'>500+ Helpline Calls</h3>
-								<p className='leader-text text-uppercase nomargin'>and 195 hours of online peer-to-peer support</p>
+								<p className='nomargin'>and 195 hours of online peer-to-peer support</p>
 							</div>
 						</div>
 					</div>
