37c31e86257952971eece80ff14bfdde8986328b
dodo721 <dodoworkspr@gmail.com>
2022-02-24 15:49:54 +0000

    get involved fixes

--- src/js/components/cards/SubscriptionBox.jsx
+++ src/js/components/cards/SubscriptionBox.jsx
@@ -65,18 +65,19 @@ export const SubscriptionForm = ({label="", product, purpose=PURPOSES.email_mail
 	return (<Form className={formClass} onSubmit={doEmailSignUp}>
 				<p className='white'><b>{label}</b></p>
 				<input type="hidden" name="purpose" value={purpose} />
-				<FormGroup className="mb-2 mr-sm-2 mb-sm-0 outer-form-group flex-grow-1 m-0 pr-md-3">
+				<FormGroup className="mb-2 mb-sm-0 outer-form-group flex-grow-1 m-0 pr-md-3 text-left mx-auto">
 					<PropControl
 						className="newsletter-email"
 						prop="email"
+						label="YOUR EMAIL"
 						path={ctaFormPath}
 						placeholder="yourname@youremail.com"
 					/>
 				</FormGroup>
 				{purpose!==PURPOSES.email_mailing_list 
-					&& <PropControl type="checkbox" path={ctaFormPath} label="Subscribe to our good news mailing list :)" prop="purpose2" value={PURPOSES.email_mailing_list} />
+					&& <PropControl type="checkbox" path={ctaFormPath} label="Subscribe to our good news mailing list and get feel-good news delivered to your inbox :)" prop="purpose2" value={PURPOSES.email_mailing_list} />
 				}
-				<Button onClick={doEmailSignUp} color="primary" disabled={ ! formData.email || hasSubmittedEmail} className="flex-grow-0">
+				<Button onClick={doEmailSignUp} color="secondary" disabled={ ! formData.email || hasSubmittedEmail} className="flex-grow-0 email-submit mt-3">
 					{buttonText}
 				</Button>				
 			</Form>);
