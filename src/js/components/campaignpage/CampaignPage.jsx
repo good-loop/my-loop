1dcfa5019d8434cceab395bb7923e0cf98c6d7d7
dodo721 <dodoworkspr@gmail.com>
2022-02-25 14:32:47 +0000

    undone mistake

--- src/js/components/campaignpage/CampaignPage.jsx
+++ src/js/components/campaignpage/CampaignPage.jsx
@@ -132,6 +132,22 @@ const fetchIHubData2_wrapAsList = pvTopItem => {
 	));
 };
 
+const JustTheBeginning = () => {
+	return <div className='w-100 bg-gl-pale-orange' >
+		<PageCard className="bg-gl-desat-blue">
+			<h1 style={{color:"white",fontWeight:'bold'}}>This is just the beginning.</h1>
+			<br/>
+			<p className='white text-center'><b>See what else we're doing and join the Good-Loop movement.</b></p>
+			<TriCards className="pt-0"
+				titles={["Tabs-for-Good", "Ad Campaigns", "Our Story"]}
+				texts={['Raise money for charity every time you open a new tab', 'Explore more examples of our campaigns', 'Meet the cofounders and discover the story of Good-Loop']}
+				images={['../img/homepage/slide-1.png', '../img/homepage/UsingAdMoneyForGood.png', '../img/homepage/amyanddaniel.png']}
+				links={['../tabsforgood', '../impactoverview', '../ourstory']}
+			/>
+		</PageCard>
+	</div>
+}
+
 /**
  * TODO support q=... flexible query
  * TODO support agency and ourselves! with multiple adverts
