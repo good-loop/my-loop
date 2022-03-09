634b26d294fb185658b80583ea0513b40f8a01a2
Wing Wong <me@nikowong.com>
2022-03-09 17:11:44 +0000

    bulid images in safari page

--- src/js/components/pages/SafariPage.jsx
+++ src/js/components/pages/SafariPage.jsx
@@ -12,26 +12,55 @@ export const SafariPage = () => {
 
 	const SlideItems = [
 		<>
-			<img src="/img/SafariPage/safari-inital.png" alt="" />
-			<p>In the Safari app  on your Mac, choose Safari Preferences, then click General.</p>
+			<h3>Step - 1</h3>
+			<div className="safari-dropdown">
+				<img src="/img/safari-preference/preferences.png" className='w-75' alt="" />
+				<img src="img/safari-preference/dropdown-preferences.png" className='dropdown-preferences' alt="" />
+			</div>
+			<p>In the Safari app on your Mac, choose Safari Preferences.</p>
 		</>,
 		<>
-			<p>In the Homepage field, enter a the Tabs For Good address:</p>
-			<p>
-				<code>https://my.good-loop.com/newtab.html</code>
-				<button onClick={() => {navigator.clipboard.writeText('https://my.good-loop.com/newtab.html')}} 
-					className='btn btn-seconday'>Copy To Clipboard</button>
-			</p>
+			<h3>Step - 2</h3>
+			<img src="/img/safari-preference/safari-final.png" className='w-75' alt="" />
+			<p>Click General tab (first tab) in your Safari Preferences.</p>
 		</>,
 		<>
-			<img src="/img/SafariPage/safari-final.png" alt="" />
+			<h3>Step - 3</h3>
+			<div className="safari-dropdown">
+				<img src="/img/safari-preference/safari-final.png" className='w-75' alt="" />
+				<img src="img/safari-preference/new-window.png" className='new-window' alt="" />
+			</div>
+		<p>Make sure your Safari opens with "A new window"</p>
+	</>,
+		<>
+			<h3>Step - 4</h3>
+			<div className="safari-dropdown">
+				<img src="/img/safari-preference/safari-final.png" className='w-75' alt="" />
+				<img src="img/safari-preference/homepage.png" className='set-homepage' alt="" />
+			</div>
 			<p>Click the “New windows open with” pop-up menu, then choose Homepage.</p>
 			<p>Click the “New tabs open with” pop-up menu, then choose Homepage.</p>
 		</>,
+		<>
+			<h3>Step - 5</h3>
+			<div className="safari-dropdown">
+				<img src="/img/safari-preference/safari-final.png" className='w-75' alt="" />
+				<img src="img/safari-preference/safari-paste.png" className='safari-paste' alt="" />
+			</div>
+			<p>In the Homepage field, enter a the Tabs For Good address:</p>
+			<code>https://my.good-loop.com/newtab.html</code> <br/>
+			<button onClick={() => {navigator.clipboard.writeText('https://my.good-loop.com/newtab.html')}} 
+				className='btn btn-primary'>Copy To Clipboard</button>
+		</>,
+		<>
+			<h3>All Done</h3>
+			<img src="/img/safari-preference/safari-final.png" className='w-75' alt="" />
+			<p>You're ready to use Tabs For Good and start raising money for good causes - just by browsing the internet.</p>
+		</>
 	];
 
 	const slides = SlideItems.map((content, i) => (
-		<div key={i} style={{height:'25em'}} className='p-5 text-center'>
+		<div key={i} style={{height:'40em'}} className='p-5 text-center'>
 			{content}
 		</div>
 	));
