4dda80efba5804a2f2d20253f962445b77285fb7
dodo721 <dodoworkspr@gmail.com>
2022-03-23 13:30:37 +0000

    Fix for blogpost endpoint

--- src/js/plumbing/ServerIO.js
+++ src/js/plumbing/ServerIO.js
@@ -120,7 +120,7 @@ ServerIO.mixPanelTrack = ({mixPanelTag, data = {}}) => {
 // override for (ad)vert
 let super_geft = ServerIO.getEndpointForType;
 ServerIO.getEndpointForType = (type) => {
-	if (C.TYPES.isBlogPost(type)) return 'http://localportal.good-loop.com/blogpost';
+	if (C.TYPES.isBlogPost(type)) return `${ServerIO.PORTAL_ENDPOINT}/blogpost`;
 	return super_geft(type);
 };
 
