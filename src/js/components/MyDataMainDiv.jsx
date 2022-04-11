4482d0ee9afe362f68d9413ce7287b1b960e6ead
Wing Wong <wing@good-loop.com>
2022-04-11 17:13:42 +0100

    cleanup

--- src/js/components/MyDataMainDiv.jsx
+++ src/js/components/MyDataMainDiv.jsx
@@ -13,36 +13,17 @@ import Crud from '../base/plumbing/Crud'; // Crud is loaded here to init (but no
 import {setShowLogin} from '../base/components/LoginWidget';
 
 // Pages
-import MyCharitiesPage from './pages/MyCharitiesPage';
-import MyAdCampaignsPage from './pages/MyAdCampaignsPage';
-import GetInvolvedPage from './pages/GetInvolvedPage';
-import CampaignPage from './campaignpage/CampaignPage';
 import E404Page from '../base/components/E404Page';
-import AccountPage from './pages/AccountPage';
 import Footer from './Footer';
-import MyGLAboutPage from './MyGLAboutPage';
 import SubscriptionBox from './cards/SubscriptionBox';
 import { addDataCredit, addFunderCredit } from '../base/components/AboutPage';
 import ServerIO from '../plumbing/ServerIO';
-import AllowlistUs from './pages/AllowlistUs';
 import MainDivBase from '../base/components/MainDivBase';
 import {A, initRouter} from '../base/plumbing/glrouter';
 import HomePage from './pages/HomePage';
-// import RedesignPage from './pages/RedesignPage';
-import GreenDashboard from './pages/GreenDashboard';
-import GreenLanding from './pages/GreenLanding';
-import TabsForGoodLandingPage from './pages/TabsForGoodLandingPage';
-import CharityLandingPage from './pages/CharityLandingPage';
-import ProductsOverviewPage from './pages/ProductsOverviewPage';
-import BlogPage from './pages/BlogPage';
-// import ImpactOverviewPage from './pages/ImpactOverviewPage';
-import OurStoryPage from './pages/OurStoryPage';
-import ForBusinessPage from './pages/ForBusinessPage';
-import ForCharityPage from './pages/ForCharityPage';
+import MyDataSignUpPage from './pages/MyDataSignUpPage';
 import { T4GCTA, T4GSignUpModal, T4GPluginButton } from './T4GSignUp';
 import { MyLoginWidgetGuts } from './MyLoginWidgetGuts';
-import BlogContent from './pages/BlogContent';
-import SafariPage from './pages/SafariPage';
 // import TestPage from './pages/TestPage';
 
 // DataStore
@@ -51,44 +32,11 @@ C.setupDataStore();
 // Person from profiler
 ServerIO.USE_PROFILER = true;
 
-/**
- * Subscribe box as a single page??
- */
-const SubscribePage = ({}) => {
-	return (<>
-		<div className="Subscribe widepage">
-			<SubscriptionBox title="Subscribe to our monthly newsletter" className="bg-gl-light-red big-sub-box"/>
-		</div>
-	</>);
-}
-
 // Actions
 
 const PAGES = {
-	blog: BlogPage, // TODO
-	// blogcontent: BlogContent,
-	campaign: CampaignPage,
-	impact: CampaignPage,
-	impactoverview: MyAdCampaignsPage,
-	// test: TestPage,
-	account: AccountPage,
-	charities: MyCharitiesPage,
-	charity: CharityLandingPage,
-	ads: MyAdCampaignsPage,
-	involve: GetInvolvedPage,
-	subscribe: SubscribePage,
-	about: MyGLAboutPage,
-	productsoverview: ProductsOverviewPage,
-	tabsforgood: TabsForGoodLandingPage,
-	allowlist: AllowlistUs,
-
 	home: HomePage,
-	greendash: GreenDashboard,
-	green: GreenLanding,
-	ourstory: OurStoryPage,
-	forbusiness: ForBusinessPage,
-	forcharity: ForCharityPage,
-	safari: SafariPage,
+	signup: MyDataSignUpPage,
 };
 // ?? switch to router??
 // const ROUTES = {
@@ -106,31 +54,22 @@ Login.dataspace = C.app.dataspace;
 const MainDiv = () => {
 
 	const navPageLinks = {
-		"ourstory":[],
-		"our-impact": ['charities', 'impactoverview', 'green'],
-		'tabsforgood':[],
 	};
 
 	const navPageLabels = {
-		"Our Story":[],
-		"Our Impact": ['Charity Impact', 'Impact Hub', 'Green Media'],
-		"Tabs for Good":[],
 	};
 
 	// HACK hide whilst we finish it
-	if ( ! Roles.isTester()) {
+	// if ( ! Roles.isTester()) {
 
-		delete navPageLinks["our-impact"].green;
-		delete navPageLabels["Our Impact"]["Green Media"];
-
-		// delete navPageLinks["blog"];
-		// delete navPageLabels["Blog"];
+	// 	delete navPageLinks["our-impact"].green;
+	// 	delete navPageLabels["Our Impact"]["Green Media"];
 		
-	}
+	// }
 
 	return (<MainDivBase
 		pageForPath={PAGES}
-		defaultPage='home'
+		defaultPage='signup'
 		navbarPages={navPageLinks}
 		navbarLabels={navPageLabels}
 		navbarDarkTheme={false}
@@ -138,7 +77,7 @@ const MainDiv = () => {
 		navbarBackgroundColour="white"
 		NavExpandSize="md"
 		// navbarLabels={getNavbarLabels}
-		fullWidthPages={["impact", 'home', 'charity', 'tabsforgood', 'account', 'green', 'charities', 'impactoverview', 'campaign', 'ourstory', 'ads', 'blog', 'blogcontent', 'allowlist', 'safari']}
+		fullWidthPages={["home"]}
 		//undecoratedPages={["blogcontent"]}
 		Footer={Footer}
 		canRegister
