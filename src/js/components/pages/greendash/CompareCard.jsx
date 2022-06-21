649b390cbeccf913395fd30cf3876003e90c599d
Roscoe <roscoe@good-loop.com>
2022-06-21 21:14:54 +0100

    Green dashboard: compare campaigns reinstated (be test@multicampaign.com to see)

--- src/js/components/pages/greendash/CompareCard.jsx
+++ src/js/components/pages/greendash/CompareCard.jsx
@@ -6,6 +6,10 @@ import NewChartWidget from '../../NewChartWidget';
 import { dataColours, getPeriodQuarter, GreenCard, GreenCardAbout, ModeButton, printPeriod, TONNES_THRESHOLD } from './dashutils';
 import { isoDate } from '../../../base/utils/miscutils';
 import { getCarbon, getSumColumn } from './carboncalc';
+import { getDataList } from '../../../base/plumbing/Crud';
+import C from '../../../C';
+import KStatus from '../../../base/data/KStatus';
+import { getId } from '../../../base/data/DataClass';
 
 
 const dummyDataCampaign = {
@@ -18,18 +22,20 @@ const dummyDataCampaign = {
 
 const baseOptions = {
 	indexAxis: 'y',
-	plugins: { legend: { display: false } },
 	scales: { x: { ticks: { callback: v => `${v} kg` } } },
+	plugins: {
+		legend: { display: false },
+		tooltip: { callbacks: { label: ctx => `${printer.prettyNumber(ctx.raw)} kg CO2` } },
+	},
 };
 
 
-const QuartersCard = ({tags, baseFilters}) => {
+const QuartersCard = ({baseFilters}) => {
 	// Set up base chart data object
 	const [chartProps, setChartProps] = useState(() => ({
 		data: {
 			labels: ['', '', '', ''],
 			datasets: [{
-				label: 'CO2',
 				data: [0, 0, 0, 0],
 			}]
 		},
@@ -65,6 +71,7 @@ const QuartersCard = ({tags, baseFilters}) => {
 						// This is the first data point above the threshold: Scale down all points & change tick labels from kg to tonnes
 						nextProps.data.datasets[0].data = nextProps.data.datasets[0].data.map(d => d / 1000);
 						nextProps.options.scales.x.ticks.callback = v => `${v} t`;
+						nextProps.options.plugins.tooltip.callbacks.label = ctx => `${printer.prettyNumber(ctx.raw)} tonnes CO2`
 						nextProps.tonnes = true;
 					}
 
@@ -78,28 +85,90 @@ const QuartersCard = ({tags, baseFilters}) => {
 	}, []);
 
 	if (!chartProps) return <Misc.Loading text="Fetching data for previous quarters..." />
-	return <NewChartWidget type="bar" data={chartProps.data} options={chartProps.options} />
+
+	return <NewChartWidget type="bar" {...chartProps} />
 };
 
 
-const CampaignCard = ({}) => {
-	return <NewChartWidget type="bar" data={dummyDataCampaign} options={baseOptions} />
+/** Transform a GreenCalcServlet data table pertaining to a campaign & insert it in the chart data object */
+const insertCampaignData = (prevProps, campaign, table) => {
+	const nextProps = _.cloneDeep(prevProps);
+	const labels = [...nextProps.data.labels];
+	const data = [...nextProps.data.datasets[0].data];
+
+	// Insert new values in sorted position, order alphabetically by campaign name
+	const insertIndex = labels.length ? labels.findIndex(label => label > campaign.name) : 0;
+	labels.splice(insertIndex, 0, campaign.name);
+	data.splice(insertIndex, 0, getSumColumn(table, 'totalEmissions'));
+
+	nextProps.data.labels = labels;
+	nextProps.data.datasets[0].data = data;
+
+	return nextProps;
+};
+
+
+const CampaignCard = ({baseFilters, campaignIds}) => {
+	const [chartProps, setChartProps] = useState(() => ({
+		data: {
+			labels: [],
+			datasets: [{ data: [] }]
+		},
+		options: baseOptions,
+	}));
+
+	useEffect(() => {
+		getDataList({
+			type: C.TYPES.Campaign,
+			status: KStatus.PUB_OR_DRAFT,
+			ids: campaignIds
+		}).promise.then(({hits: campaigns}) => {
+			campaigns.forEach(campaign => {
+				getCarbon({...baseFilters, q: `campaign:${getId(campaign)}`, nocache: true}).promise.then(res => {
+					setChartProps(prev => insertCampaignData(prev, campaign, res.table));
+				});
+			})
+		});
+	}, [campaignIds]);
+
+	// if (chartProps.data.labels.length < campaignIds.length) return
+
+	return <NewChartWidget type="bar" {...chartProps} />
 };
 
 
 const CompareCard = (props) => {
 	const [mode, setMode] = useState('quarter');
+	const [campaignIds, setCampaignIds] = useState([]);
+
+	// Get available (shared-with-user) campaigns to decide whether compare-campaigns mode is available
+	useEffect(() => {
+		Login.getSharedWith().then(({cargo: shareList}) => {
+			if (!shareList) return;
+
+			const nextCampaignIds = [];
+			shareList.forEach(share => {
+				const matches = share.item.match(/^Campaign:(\w+)/);
+				if (matches) nextCampaignIds.push(matches[1]);
+			});
+			setCampaignIds(nextCampaignIds);
+		});
+	}, [Login.getId()])
 
 	const subcard = (mode === 'quarter') ? (
 		<QuartersCard {...props} />
 	) : (
-		<CampaignCard {...props} />
+		<CampaignCard {...props} campaignIds={campaignIds} />
 	);
 
 	return <GreenCard title="How do your ad emissions compare?" className="carbon-compare">
 		<div className="d-flex justify-content-around mb-2">
-			<ModeButton name="quarter" mode={mode} setMode={setMode}>Quarter</ModeButton>
-			<ModeButton disabled name="campaign" mode={mode} setMode={setMode}>Campaign</ModeButton>
+			<ModeButton name="quarter" mode={mode} setMode={setMode}>
+				Quarter
+			</ModeButton>
+			<ModeButton name="campaign" mode={mode} setMode={setMode} disabled={campaignIds.length <= 1}>
+				Campaign
+			</ModeButton>
 		</div>
 		{subcard}
 		<GreenCardAbout>
