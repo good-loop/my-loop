d8e4af3ddd501d7f777aad57195aa8e6a831fa9c
Roscoe <roscoe@good-loop.com>
2022-09-27 16:39:50 +0100

    Green dashboard testing: baker/test should use greencalc?server=production on testportal to get latest calc code

---
+++ config/baker.js
@@ -0,0 +1,15 @@
+// Configuration for portal on `gravitas` (RM dev laptop)
+
+// Change to "local", "test" or "" to switch all endpoints together
+const cluster = 
+	'test';
+	// 'local';
+	// ''; // if you want production!
+
+const protocol = (cluster === 'local') ? 'http' : 'https';
+
+module.exports = {
+	ServerIOOverrides: {
+		GREENCALC_ENDPOINT: `https://testportal.good-loop.com/greencalc?server=production`,
+	}
+};
