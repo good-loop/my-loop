2d22d1a35bc5c7717c1041056712b5b302547104
Wing <wing@good-loop.com>
2022-10-20 12:54:01 +0100

    Oct 2022: Weird bug, pvChartData will return incorrect extra data in the buckets (e.g. AU-QLD)

--- src/js/components/pages/greendash/MapCardEmissions.jsx
+++ src/js/components/pages/greendash/MapCardEmissions.jsx
@@ -9,7 +9,6 @@ import { getCarbon } from './carboncalc';
 import { dataColours, GreenCard } from './dashutils';
 import { getCarbonEmissions } from './emissionscalc';
 
-
 /**
  * To extract path bounding box centres from an onscreen SVG (in SVG coords)...
 Array.from(document.querySelectorAll('.map-svg path')).reduce((acc, path) => {
@@ -25,39 +24,42 @@ Array.from(document.querySelectorAll('.map-svg path')).reduce((acc, path) => {
  * e.g. Alaska and Hawaii putting the centre of the U.S. in the northern Pacific.
  */
 
-
 // When querying each country, what sub-location type should we breakdown on?
-const subLocationForCountry = (country) => ({
-	UK: 'locn_sub2', // Counties
-	// We default to locn_sub1, so we can omit these - keep as comments for reference.
-	// US: 'locn_sub1', // States
-	// AU: 'locn_sub1', // States
-	// CA: 'locn_sub1', // States
-}[country] || 'locn_sub1');
-
+const subLocationForCountry = (country) =>
+	({
+		UK: 'locn_sub2', // Counties
+		// We default to locn_sub1, so we can omit these - keep as comments for reference.
+		// US: 'locn_sub1', // States
+		// AU: 'locn_sub1', // States
+		// CA: 'locn_sub1', // States
+	}[country] || 'locn_sub1');
 
 /** Map regions not in the dataset (because there are no events for them)
  * should be the same colour as regions with value 0*/
 const zeroFill = dataColours([0, 1])[0];
 
-
 const MapDownloadCSV = ({ data, mapDefs }) => {
 	if (!data || !mapDefs) return 'CSV';
 
 	const cols = [
-		new Column({Header: 'Region', accessor: 'name'}),
-		new Column({Header: 'Region ISO code', accessor: 'id'}),
-		new Column({Header: 'Impressions', accessor: 'impressions'}),
-		new Column({Header: 'CO2e (Kg)', accessor: 'carbon'}),
+		new Column({ Header: 'Region', accessor: 'name' }),
+		new Column({ Header: 'Region ISO code', accessor: 'id' }),
+		new Column({ Header: 'Impressions', accessor: 'impressions' }),
+		new Column({ Header: 'CO2e (Kg)', accessor: 'carbon' }),
 	];
 
-	const tableData = Object.entries(data).map(([id, {impressions, carbon}]) => {
-		return { name: mapDefs.regions[id]?.name || 'unknown', id, impressions, carbon };
-	}).sort((a, b) => (a.name || '').localeCompare(b.name));
+	const tableData = Object.entries(data)
+		.map(([id, { impressions, carbon }]) => {
+			return { name: mapDefs.regions[id]?.name || 'unknown', id, impressions, carbon };
+		})
+		.sort((a, b) => (a.name || '').localeCompare(b.name));
 
-	return <DownloadCSVLink columns={cols} data={tableData}>CSV</DownloadCSVLink>;
+	return (
+		<DownloadCSVLink columns={cols} data={tableData}>
+			CSV
+		</DownloadCSVLink>
+	);
 };
-
 
 const SVGMap = ({ mapDefs, data, setFocusRegion, svgRef, showLabels }) => {
 	const [pathCentres, setPathCentres] = useState({}); // Estimate region centres from bounding boxes to place text labels
@@ -69,7 +71,7 @@ const SVGMap = ({ mapDefs, data, setFocusRegion, svgRef, showLabels }) => {
 	let labels = [];
 
 	Object.entries(mapDefs.regions).forEach(([id, props]) => {
-		let { carbon = 0, colour = zeroFill } = (data?.[id] || {});
+		let { carbon = 0, colour = zeroFill } = data?.[id] || {};
 		let unit = 'kg';
 		if (carbon >= 1000) {
 			carbon /= 1000;
@@ -77,7 +79,7 @@ const SVGMap = ({ mapDefs, data, setFocusRegion, svgRef, showLabels }) => {
 		}
 
 		// Don't modify base map with applied fill/stroke!
-		props = { ...props, fill: colour, stroke: '#fff', strokeWidth: mapDefs.svgAttributes.fontSize / 10};
+		props = { ...props, fill: colour, stroke: '#fff', strokeWidth: mapDefs.svgAttributes.fontSize / 10 };
 
 		// Don't paint misleading colours on a map we don't have data for
 		if (loading) {
@@ -87,57 +89,69 @@ const SVGMap = ({ mapDefs, data, setFocusRegion, svgRef, showLabels }) => {
 
 		// Countries are clickable, sublocations aren't.
 		if (setFocusRegion) {
-			props.style = {cursor: 'pointer'};
+			props.style = { cursor: 'pointer' };
 			props.onClick = () => setFocusRegion(id);
 		}
 
 		const pathId = `${mapDefs.id}-${id}`;
 
 		// Find the centre of the region's bounding box to position a text label on the bigger map
-		const pathRef = showLabels ? (path) => {
-			if (!path) return;
-			
-			setPathCentres(prev => {
-				if (prev[pathId]) return prev;
-				const bbox = path.getBBox();
+		const pathRef = showLabels
+			? (path) => {
+					if (!path) return;
+					setPathCentres((prev) => {
+						if (prev[pathId]) return prev;
+						const bbox = path.getBBox();
 
-				return {
-					...prev,
-					[pathId]: { cx: bbox.x + (bbox.width / 2), cy: bbox.y + (bbox.height / 2) }
-				};
-			});
-		} : null;
+						return {
+							...prev,
+							[pathId]: { cx: bbox.x + bbox.width / 2, cy: bbox.y + bbox.height / 2 },
+						};
+					});
+			  }
+			: null;
 
 		// Tooltip on hover
-		const title = loading ? null : <title>{props.name}: {carbon.toFixed(2)} {unit} CO2e</title>;
+		const title = loading ? null : (
+			<title>
+				{props.name}: {carbon.toFixed(2)} {unit} CO2e
+			</title>
+		);
 
-		regions.push(<path key={`path-${id}`} data-id={id} {...props} ref={pathRef}>{title}</path>);
+		regions.push(
+			<path key={`path-${id}`} data-id={id} {...props} ref={pathRef}>
+				{title}
+			</path>
+		);
 
 		// Skip labels for regions with less than 100g carbon output. Harsh, I know
 		if (showLabels && carbon > 0.1 && pathCentres[pathId]) {
-			let {cx, cy} = {...pathCentres[pathId], ...props};
+			let { cx, cy } = { ...pathCentres[pathId], ...props };
 			const transY = mapDefs.svgAttributes.fontSize / 2;
-			
-			labels.push(<g key={`label-${id}`}>
-				<text className="map-label-name" x={cx} y={cy} textAnchor="middle" transform={`translate(0 ${-transY})`} fontWeight="600">
-					{props.name}
-				</text>
-				<text className="map-label-carbon" x={cx} y={cy} textAnchor="middle" transform={`translate(0 ${transY})`}>
-					{carbon.toFixed(2)} {unit}
-				</text>
-			</g>);
+
+			labels.push(
+				<g key={`label-${id}`}>
+					<text className='map-label-name' x={cx} y={cy} textAnchor='middle' transform={`translate(0 ${-transY})`} fontWeight='600'>
+						{props.name}
+					</text>
+					<text className='map-label-carbon' x={cx} y={cy} textAnchor='middle' transform={`translate(0 ${transY})`}>
+						{carbon.toFixed(2)} {unit}
+					</text>
+				</g>
+			);
 		}
 	});
 
-	return <div className="map-container text-center">
-		<svg className="map-svg" version="1.1" {...mapDefs.svgAttributes} xmlns="http://www.w3.org/2000/svg" ref={svgRef}>
-			{regions}
-			{labels}
-		</svg>
-		{loading && <Misc.Loading text={`Fetching carbon data for ${mapDefs.name}`} />}
-	</div>;
+	return (
+		<div className='map-container text-center'>
+			<svg className='map-svg' version='1.1' {...mapDefs.svgAttributes} xmlns='http://www.w3.org/2000/svg' ref={svgRef}>
+				{regions}
+				{labels}
+			</svg>
+			{loading && <Misc.Loading text={`Fetching carbon data for ${mapDefs.name}`} />}
+		</div>
+	);
 };
-
 
 const MapCardEmissions = ({ baseFilters }) => {
 	const [mapData, setMapData] = useState('loading'); // Object mapping region ID to imps + carbon
@@ -147,8 +161,8 @@ const MapCardEmissions = ({ baseFilters }) => {
 	const [error, setError] = useState(); // Problems loading map?
 	const [popOut, setPopOut] = useState(false); // Pop out card for larger map?
 
-	const isWorld = (focusRegion === 'world');
-	const mapDefsReady = mapDefs && (mapDefs.id === focusRegion);
+	const isWorld = focusRegion === 'world';
+	const mapDefsReady = mapDefs && mapDefs.id === focusRegion;
 
 	// Fetch the JSON with the map data for the current focus country
 	useEffect(() => {
@@ -158,18 +172,20 @@ const MapCardEmissions = ({ baseFilters }) => {
 			setError(`No detailed map available for country code "${focusRegion}"`);
 		};
 
-		fetch(`/js-data/mapdefs-${focusRegion}.json`)
-			.then(res => {
-				if (!res.ok) {
-					onError();
-					return;
-				}
-				res.json().then(json => {
+		fetch(`/js-data/mapdefs-${focusRegion}.json`).then((res) => {
+			if (!res.ok) {
+				onError();
+				return;
+			}
+			res
+				.json()
+				.then((json) => {
 					setMapDefs(json);
 					// clear error on successfully loading a country map
 					if (!isWorld) setError(null);
-				}).catch(onError);
-			})
+				})
+				.catch(onError);
+		});
 	}, [focusRegion]);
 
 	// Which location or sub-location type do we want? Country, sub-1, sub-2?
@@ -184,7 +200,7 @@ const MapCardEmissions = ({ baseFilters }) => {
 		filters.q = SearchQuery.setPropOr(new SearchQuery(filters.q), 'country', [focusRegion]).query;
 		filters.focusCountry = focusRegion;
 		filters.subLocationField = locationField;
-	};
+	}
 
 	const pvChartData = getCarbonEmissions(filters);
 
@@ -193,63 +209,94 @@ const MapCardEmissions = ({ baseFilters }) => {
 		if (!pvChartData.value) {
 			setMapData('loading');
 			return;
-		};
+		}
 		// Don't process data using metadata for wrong map
 		if (!mapDefs || !mapDefsReady) return;
 
 		// Country or sub-location breakdown?
-		const locnBuckets = pvChartData.value["by_" + locationField].buckets;
+		const locnBuckets = pvChartData.value['by_' + locationField].buckets;
 
-		// Rename locations with no corresponding map entry to OTHER
-		// convert old non-namespaced sublocations e.g. 'CA' => 'US-CA'
-		Object.values(locnBuckets).forEach(val => {
-			if (!mapDefs.regions[val.key] && mapDefs.id !== 'world') {
-				const fixedKey = `${focusRegion}-${val.key}`;
-				val.key = fixedKey;
-				locnBuckets.push = val;
+		// Oct 2022: Weird bug, pvChartData will return incorrect extra data in the buckets (e.g. AU-QLD)
+		Object.values(locnBuckets).forEach((val, index) => {
+			if (mapDefs.id !== 'world' && val.key.startsWith(mapDefs.id)) {
+				delete locnBuckets[index];
 			}
 		})
 
+		// Rename locations with no corresponding map entry to OTHER
+		// convert old non-namespaced sublocations e.g. 'CA' => 'US-CA'
+		Object.values(locnBuckets).forEach((val) => {
+			if (mapDefs.id !== 'world' && !mapDefs.regions[val.key]) {
+				val.key = `${focusRegion}-${val.key}`;
+			}
+		});
+
+		console.log('debug- locnBuckets after', locnBuckets);
+
 		// assign colours
-		const colours = dataColours(locnBuckets.map(row => row.co2));
+		const colours = dataColours(locnBuckets.map((row) => row.co2));
 		// zip colours, states, carbon together for the map
-		setMapData(locnBuckets.reduce((acc, row, i) => {
-			acc[row.key] = { colour: colours[i], impressions: row.count, carbon: row.co2 };
-			return acc;
-		}, {}));
+		setMapData(
+			locnBuckets.reduce((acc, row, i) => {
+				acc[row.key] = { colour: colours[i], impressions: row.count, carbon: row.co2 };
+				return acc;
+			}, {})
+		);
 	}, [JSON.stringify(filters), pvChartData.value, mapDefs]);
 
-	const cardContents = <>
-		<div className="mb-2 text-center">
-			<strong>{mapDefs?.name}</strong>
-		</div>
-		<SVGMap setFocusRegion={isWorld && setFocusRegion} mapDefs={mapDefs} data={mapData} svgRef={setSvgEl} loading={!mapData} showLabels={popOut} />
-		<div className="mt-2 map-controls">
-			<span className="pull-left">
-				{error ? <small>{error}</small> : <>
-					Download{' '}
-					<a download={`green-map-${focusRegion}.svg`} href={`data:image/svg+xml,${encodeURIComponent(svgEl?.outerHTML)}`}>map</a>{' / '}
-					<MapDownloadCSV data={mapData} mapDefs={mapDefs} />
-				</>}
-			</span>
-			<span className="pull-right">
-				{isWorld ? 'Click to focus' : (
-					mapDefsReady && <a href="#" onClick={(e) => { stopEvent(e); setFocusRegion('world');}}>Back</a>
-				)}
-			</span>
-		</div>
-	</>;
+	console.log('debug- mapData', mapData);
+
+	const cardContents = (
+		<>
+			<div className='mb-2 text-center'>
+				<strong>{mapDefs?.name}</strong>
+			</div>
+			<SVGMap setFocusRegion={isWorld && setFocusRegion} mapDefs={mapDefs} data={mapData} svgRef={setSvgEl} loading={!mapData} showLabels={popOut} />
+			<div className='mt-2 map-controls'>
+				<span className='pull-left'>
+					{error ? (
+						<small>{error}</small>
+					) : (
+						<>
+							Download{' '}
+							<a download={`green-map-${focusRegion}.svg`} href={`data:image/svg+xml,${encodeURIComponent(svgEl?.outerHTML)}`}>
+								map
+							</a>
+							{' / '}
+							<MapDownloadCSV data={mapData} mapDefs={mapDefs} />
+						</>
+					)}
+				</span>
+				<span className='pull-right'>
+					{isWorld
+						? 'Click to focus'
+						: mapDefsReady && (
+								<a
+									href='#'
+									onClick={(e) => {
+										stopEvent(e);
+										setFocusRegion('world');
+									}}
+								>
+									Back
+								</a>
+						  )}
+				</span>
+			</div>
+		</>
+	);
 
 	return (
-		<GreenCard title="Where are your emissions produced?" className="carbon-map flex-column" downloadable={false}>
-			<div role="button" className="pop-out-button" onClick={() => setPopOut(true)}>⇱</div>
+		<GreenCard title='Where are your emissions produced?' className='carbon-map flex-column' downloadable={false}>
+			<div role='button' className='pop-out-button' onClick={() => setPopOut(true)}>
+				⇱
+			</div>
 			{!popOut && cardContents}
-			<Modal className="carbon-map" isOpen={popOut} toggle={() => setPopOut(!popOut)} size="xl" >
+			<Modal className='carbon-map' isOpen={popOut} toggle={() => setPopOut(!popOut)} size='xl'>
 				<ModalBody>{popOut && cardContents}</ModalBody>
 			</Modal>
 		</GreenCard>
 	);
 };
-
 
 export default MapCardEmissions;
