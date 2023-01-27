b72e1b68f66c6c0c0ebab1a6487b4ad7cf6382ed
Wing <wing@good-loop.com>
2023-01-27 14:34:58 +0000

    test_create_sharable_link

--- src/python/__tests__/ShareLoginTest.py
+++ src/python/__tests__/ShareLoginTest.py
@@ -15,8 +15,11 @@ class ShareLoginTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    def test_login(self):
-        'Login in green dashboard and see impressions'
+    def test_create_sharable_link(self):
+        '''
+        Bug: youagain will not let you create a new sharable link. 
+        500: Already registered and name/password incorrect for Campaign:xxxxxxxx_by_xxxx@good-loop.com@email (app: my.good-loop.com) 
+        '''
 
         driver = self.driver
         driver.delete_all_cookies()
@@ -33,12 +36,25 @@ class ShareLoginTest(unittest.TestCase):
         self.assertIn("Impressions", impressions.text, "Logged in and see impressions")
 
         # Get share links
-        print(driver.get_cookies())
         driver.find_element(By.ID, 'share-btn').click()
         time.sleep(1)
         driver.find_element(By.ID, 'copy-share-dashboard-link').click()
-        time.sleep(5)
+        time.sleep(1)
+        for log in driver.get_log('browser'): 
+            if log['level'] == 'SEVERE' and log['message'].startswith('https://youagain.good-loop.com/youagain.json'):
+                self.assertNotIn('500', log['message'], "Failed to create sharable dashboard link in youagain server")
 
+    # def test_share_link_cookies_bug(self):
+    #     driver = self.driver
+    #     driver.delete_all_cookies()
+    #     driver.get(TEST_URL)
+
+    #     self.assertIn("Good-Loop", driver.title, "Login page loaded")
+
+    #     driver.find_element(By.ID, 'loginByEmail-email').send_keys(email)
+    #     driver.find_element(By.ID, 'loginByEmail-password').send_keys(password)
+    #     driver.find_element(By.ID, 'loginByEmail-submit').click()
+    #     time.sleep(1)
 
 if __name__ == '__main__':
     email = 'wing@good-loop.com'
