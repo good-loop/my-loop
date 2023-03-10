9e142dc01e0fd47fada24574bb404beca5874ac1
dodo721 <dodoworkspr@gmail.com>
2023-03-10 16:53:28 +0000

    Removed redundant component

--- src/js/components/impact/ImpactOverviewPage.jsx
+++ src/js/components/impact/ImpactOverviewPage.jsx
@@ -217,28 +217,6 @@ const BrandDonationInfo = ({brand}) => {
 	</GLHorizontal>;
 }
 
-const ErrorDisplay = ({e}) => {
-
-	const [showError, setShowError] = useState(false);
-
-	let errorTitle = "Sorry, something went wrong :(";
-
-	if (e.message.includes("404: Not found")) errorTitle = "404: We couldn't find that!"
-	if (e.message.includes("Invalid URL")) errorTitle = "Sorry, that's not a valid page!"
-
-	return <Container className='mt-5'>
-		<h1>{errorTitle}</h1>
-		<p>
-			Check you have the correct URL. If you think this is a bug, please report it to support@good-loop.com
-		</p>
-		<CardCollapse title="Error" collapse={!showError} onHeaderClick={() => setShowError(!showError)}>
-			<code>
-				{e.message}
-			</code>
-		</CardCollapse>
-	</Container>;
-}
-
 const LogosDisplay = ({brand, subBrands}) => {
 
 	const BrandLogo = ({item}) => {
