1e3c9384fd2db1e731ab0541890b5b8c589a1090
Wing Wong <me@nikowong.com>
2023-03-27 16:04:21 +0800

    chart part done

--- src/js/components/pages/greendash/GreenRecommendation.tsx
+++ src/js/components/pages/greendash/GreenRecommendation.tsx
@@ -21,6 +21,8 @@ interface ResolvedPromise extends ByDomainValue {
 	sampling?: ByDomainValue;
 }
 
+const TICKS_NUM = 150;
+
 const GreenRecommendation2 = (): JSX.Element | null => {
 	const urlParams = paramsFromUrl(['period', 'prob', 'sigfig', 'nocache']);
 	const period = urlParams.period;
@@ -43,41 +45,39 @@ const GreenRecommendation2 = (): JSX.Element | null => {
 	 */
 	const baseFilterConfirmed = { ...baseFilters, numRows: '10000' } as unknown as BaseFilters;
 
-	const scaledBuckets = useMemo(() => {
-		const pvChartTotal = getCarbon({ ...baseFilterConfirmed, breakdown: ['domain{"countco2":"sum"}'] });
-
-		if (pvChartTotal.resolved) {
-			const pvChartTotalValue = baseFilterConfirmed.prob && baseFilterConfirmed.prob != 0 ? pvChartTotal.value?.sampling : pvChartTotal.value;
-			const bucketsPer1000 = emissionsPerImpressions(pvChartTotalValue.by_domain.buckets);
-
-			const co2s = bucketsPer1000.map((row) => row.co2! as number);
-			const maxCo2 = Math.max(...co2s);
-			const minCo2 = Math.min(...co2s);
-			const steps = (maxCo2 - minCo2) / 100; // How large is a tick
-
-			const scaledBuckets = bucketsPer1000.map((row) => {
-				const percentage = Math.floor(((row.co2 as number) - minCo2) / steps);
-				return { key: row.key, count: row.count, co2: row.co2, percentage };
-			});
-
-			return scaledBuckets;
-		}
-	}, [baseFilterConfirmed]);
-
-
 	const chartData = useMemo(() => {
+		const pvChartTotal = getCarbon({ ...baseFilterConfirmed, breakdown: ['domain{"countco2":"sum"}'] });
+		if (!pvChartTotal.resolved) return;
+
+		const pvChartTotalValue = baseFilterConfirmed.prob && baseFilterConfirmed.prob != 0 ? pvChartTotal.value?.sampling : pvChartTotal.value;
+		const bucketsPer1000 = emissionsPerImpressions(pvChartTotalValue.by_domain.buckets);
+
+		const co2s = bucketsPer1000.map((row) => row.co2! as number);
+		const maxCo2 = Math.max(...co2s);
+		const minCo2 = Math.min(...co2s);
+		const steps = (maxCo2 - minCo2) / TICKS_NUM; // How large is a tick
+
+		const scaledBuckets = bucketsPer1000.map((row) => {
+			const percentage = Math.floor(((row.co2 as number) - minCo2) / steps);
+			return { key: row.key, count: row.count, co2: row.co2, percentage };
+		});
+
 		if (!scaledBuckets) return;
 
-		const percentageBuckets: typeof scaledBuckets[] = Array.from({ length: 100 }, () => []);
+		const percentageBuckets: typeof scaledBuckets[] = Array.from({ length: TICKS_NUM }, () => []);
 		scaledBuckets.forEach((row) => {
 			const percentageKey = Math.max(row.percentage, 1) - 1;
 			percentageBuckets[percentageKey].push(row);
-		})
+		});
 
-		console.log('percentageBuckets', percentageBuckets);
+		const dataLabels = percentageBuckets.map((row) =>
+			row[0]?.co2 ? (row[0].co2 as number).toPrecision(2) : (percentageBuckets.indexOf(row) * steps + minCo2).toPrecision(2)
+		);
+		const dataValues = percentageBuckets.map((row) => row.length);
 
-		const dataLabels = percentageBuckets.map(row => percentageBuckets.indexOf(row))
-		const dataValues = percentageBuckets.map(row => row.length);
+		const chartOptions = {
+			responsive: true,
+		};
 
 		return {
 			type: 'bar',
@@ -91,23 +91,29 @@ const GreenRecommendation2 = (): JSX.Element | null => {
 					},
 				],
 			},
+			options: chartOptions,
 		};
-	}, [scaledBuckets]);
+	}, [baseFilterConfirmed]);
 
-	const chartOptions = {
-		responsive: true,
-		tooltips: {
-			mode: 'ticks',
-			intersect: true,
-		},
-	};
+	const [range, setRange] = useState<[number, number]>();
 
 	return (
-		<>
-			<Row className='card-row'>
-				{chartData && <NewChartWidget options={chartOptions} width={1000} height={null} datalabels={null} maxy={null} {...chartData} />}
+		<div>
+			<Row>
+				<Col xs={2}>X Domains</Col>
+				<Col xs={8}>
+					<div className='w-100'>
+						{!chartData ? (
+							<Misc.Loading pv={null} inline={null} text={null} />
+						) : (
+							<NewChartWidget width={null} height={null} datalabels={null} maxy={null} {...chartData} />
+						)}
+					</div>
+				</Col>
+				<Col xs={2}>Y Domains</Col>
 			</Row>
-		</>
+			<Row>Sliders</Row>
+		</div>
 	);
 };
 
