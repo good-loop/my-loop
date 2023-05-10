3aad93bc51b8c8c540e99fb11222ea2610544a8c
Aidan <aidan@good-loop.com>
2023-05-10 14:58:07 +0100

    playwright test - basic setup

---
+++ playwright.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    use: {
+      headless: true,
+      viewport: { width: 1280, height: 720 },
+      ignoreHTTPSErrors: true,
+    },
+  };
+  
---
+++ tests/README.md
@@ -0,0 +1,3 @@
+# my-loop/tests - Playwright tests
+
+To run the tests: `npx playwright test`
