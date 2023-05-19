06bcea70f2d1099d21eeee50c1c835f2fa2d22e8
Wing <wing@good-loop.com>
2023-05-19 10:49:18 +0100

    logout page update

--- src/js/components/pages/LogoutPage.jsx
+++ src/js/components/pages/LogoutPage.jsx
@@ -1,21 +1,26 @@
-import React, { useEffect } from 'react';
-import Login from '../../base/youagain';
-import Misc from '../../base/components/Misc';
+import React from "react";
+import Login from "../../base/youagain";
+import Misc from "../../base/components/Misc";
 
 /**
- * A url to directly log you out. This is a hack to logout in T4G without HTTPS. Do not use this page in other use cases. 
+ * A url to directly log you out. This is a hack to logout in T4G without HTTPS. Do not use this page in other use cases.
  */
 const LogoutPage = () => {
-	const loggedinPV = Login.isLoggedIn(); // This will load after youagain loaded
+	const user = Login.getUser();
 
-	if (!loggedinPV) {
+	if (user === null) {
 		window.location.href = "/home";
 	}
-	Login.logout();
 
-  return <>
-		<Misc.Loading text={"Logging out"} />
-	</>;
+	if (user && user.auth) {
+		Login.logout();
+	}
+
+	return (
+		<>
+			<Misc.Loading text={"Logging out"} />
+		</>
+	);
 };
 
 export default LogoutPage;
