305e9d9fce8b16bc761b67f8f56a3b5728f4ebe8
Lewis <lewis.frampton@gmail.com>
2023-06-21 17:18:11 +0100

    tweaks + map impressions filtering

--- src/js/components/impact/ImpactOverviewPage.jsx
+++ src/js/components/impact/ImpactOverviewPage.jsx
@@ -761,8 +761,7 @@ const CountryViewsGLCard = ({basis, baseObjects}) => {
 		console.warn("CountryViewsGLCard - no baseObjects");
 		return null;
 	}
-	let impressionData = getImpressionsByCampaignByCountry({baseObjects})
-
+	let impressionData = getImpressionsByCampaignByCountry({baseObjects, cutoff:0.5})
 	// Prepare data for non-modal view - total impressions and countries
 	const totalCountries = Object.keys(impressionData).filter(country => country !== "unset").length;
 	const impressions = sumBy(Object.values(impressionData), 'impressions') // sum impressions over all regions
