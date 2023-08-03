9ff182cac20881421dc7a4f6e661407ae50d7823
Daniel Winterstein <daniel.winterstein@gmail.com>
2023-08-03 11:43:30 +0100

    Update GreenRecsCreative.tsx
    avoid badsitename warning on creativeHtml

--- src/js/components/pages/greendash/GreenRecsCreative.tsx
+++ src/js/components/pages/greendash/GreenRecsCreative.tsx
@@ -251,7 +251,7 @@ function CreativeOptimisationOverview({ tag, manifest }): JSX.Element {
 	}
 
 	// Website we can't currently analyse? Don't show the Analyse button (general users) or show a warning (GL users)
-	const badSiteName = tag.creativeURL? badSite(tag.creativeURL) : "creative url unset";
+	const badSiteName = tag.creativeURL? badSite(tag.creativeURL) : (tag.creativeHtml? null : "creative url unset");
 	const canAnalyse = !badSiteName || tag.creativeHtml || Roles.isTester();
 
 	return (
