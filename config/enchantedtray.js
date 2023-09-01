dcc66405f92268fefbb77289e89c0f4c39275456
Roscoe <roscoe@good-loop.com>
2023-09-01 12:08:33 +0100

    See matching wwappbase.js branch - update webpack and host configs to new format

--- config/enchantedtray.js
+++ config/enchantedtray.js
@@ -1,30 +1,23 @@
-// Configuration for portal on `gravitas` (RM dev laptop)
+// Front-end configuration for `enchantedtray` (DW dev laptop)
 
-// Change to "local", "test" or "" to switch all endpoints together
-const cluster = 
-	// 'test';
-	// 'local';
-	// 'stage';
-	''; // if you want production!
+// Change index to switch all endpoints together
+const cluster = ['', 'stage', 'test', 'local'][0];
+const protocol = 'https';
 
-const protocol = 'https';// (cluster === 'local') ? 'http' : 'https';
-
-module.exports = {
-	ServerIOOverrides: {
-		// APIBASE: `${protocol}://${cluster}portal.good-loop.com`,
-		AS_ENDPOINT: `${protocol}://${cluster}as.good-loop.com`,
-		PORTAL_ENDPOINT: `${protocol}://${cluster}portal.good-loop.com`,
-		DEMO_ENDPOINT: `${protocol}://${cluster}demo.good-loop.com`,
-		DATALOG_ENDPOINT: `${protocol}://${cluster}lg.good-loop.com/data`,
-		// DATALOG_ENDPOINT: `https://stagelg.good-loop.com/data`,
-		MEDIA_ENDPOINT: `${protocol}://${cluster}uploads.good-loop.com`,
-		// MEASURE_ENDPOINT: `${protocol}://${cluster}measure.good-loop.com/measure`,
-		MEASURE_ENDPOINT: `https://localmeasure.good-loop.com/measure`,
-		ANIM_ENDPOINT: `${protocol}://${cluster}portal.good-loop.com/_anim`,
-		CHAT_ENDPOINT: `${protocol}://${cluster}chat.good-loop.com/reply`,
-		// DATALOG_DATASPACE: 'gl',
-		// ENDPOINT_NGO: 'https://test.sogive.org/charity',
-		// JUICE_ENDPOINT: 'https://localjuice.good-loop.com',
-		// ADRECORDER_ENDPOINT: 'http://localadrecorder.good-loop.com/record',
-	}
+export const ServerIOOverrides = {
+	// APIBASE: `${protocol}://${cluster}portal.good-loop.com`,
+	AS_ENDPOINT: `${protocol}://${cluster}as.good-loop.com`,
+	PORTAL_ENDPOINT: `${protocol}://${cluster}portal.good-loop.com`,
+	DEMO_ENDPOINT: `${protocol}://${cluster}demo.good-loop.com`,
+	DATALOG_ENDPOINT: `${protocol}://${cluster}lg.good-loop.com/data`,
+	// DATALOG_ENDPOINT: `https://stagelg.good-loop.com/data`,
+	MEDIA_ENDPOINT: `${protocol}://${cluster}uploads.good-loop.com`,
+	// MEASURE_ENDPOINT: `${protocol}://${cluster}measure.good-loop.com/measure`,
+	MEASURE_ENDPOINT: `https://localmeasure.good-loop.com/measure`,
+	ANIM_ENDPOINT: `${protocol}://${cluster}portal.good-loop.com/_anim`,
+	CHAT_ENDPOINT: `${protocol}://${cluster}chat.good-loop.com/reply`,
+	// DATALOG_DATASPACE: 'gl',
+	// ENDPOINT_NGO: 'https://test.sogive.org/charity',
+	// JUICE_ENDPOINT: 'https://localjuice.good-loop.com',
+	// ADRECORDER_ENDPOINT: 'http://localadrecorder.good-loop.com/record',
 };
--- config/vdurkin-gl.js
+++ config/vdurkin-gl.js
@@ -1,27 +1,21 @@
-// Configuration for portal on `gravitas` (RM dev laptop)
+// Front-end configuration for `vdurkin-gl` (VD dev laptop)
 
-// Change to "local", "test" or "" to switch all endpoints together
-const cluster = 
-	//'test';
-	//'local';
-	''; // if you want production!
+// Change index to switch all endpoints together
+const cluster = ['', 'stage', 'test', 'local'][0];
+const protocol = 'https';
 
-const protocol = 'https';//(cluster === 'local') ? 'http' : 'https';
-
-module.exports = {
-	ServerIOOverrides: {
-		APIBASE: `${protocol}://${cluster}portal.good-loop.com`,
-		AS_ENDPOINT: `${protocol}://${cluster}as.good-loop.com`,
-		PORTAL_ENDPOINT: `${protocol}://${cluster}portal.good-loop.com`,
-		DEMO_ENDPOINT: `${protocol}://${cluster}demo.good-loop.com`,
-		DATALOG_ENDPOINT: `${protocol}://${cluster}lg.good-loop.com/data`,
-		MEDIA_ENDPOINT: `${protocol}://${cluster}uploads.good-loop.com`,
-		ANIM_ENDPOINT: `${protocol}://${cluster}portal.good-loop.com/_anim`,
-		CHAT_ENDPOINT: `${protocol}://${cluster}chat.good-loop.com/reply`,
-		PROFILER_ENDPOINT: `${protocol}://${cluster}profiler.good-loop.com`,
-		// DATALOG_DATASPACE: 'gl',
-		ENDPOINT_NGO: 'https://test.sogive.org/charity',
-		// JUICE_ENDPOINT: 'https://localjuice.good-loop.com',
-		// ADRECORDER_ENDPOINT: 'http://localadrecorder.good-loop.com/record',
-	}
+export const ServerIOOverrides = {
+	APIBASE: `${protocol}://${cluster}portal.good-loop.com`,
+	AS_ENDPOINT: `${protocol}://${cluster}as.good-loop.com`,
+	PORTAL_ENDPOINT: `${protocol}://${cluster}portal.good-loop.com`,
+	DEMO_ENDPOINT: `${protocol}://${cluster}demo.good-loop.com`,
+	DATALOG_ENDPOINT: `${protocol}://${cluster}lg.good-loop.com/data`,
+	MEDIA_ENDPOINT: `${protocol}://${cluster}uploads.good-loop.com`,
+	ANIM_ENDPOINT: `${protocol}://${cluster}portal.good-loop.com/_anim`,
+	CHAT_ENDPOINT: `${protocol}://${cluster}chat.good-loop.com/reply`,
+	PROFILER_ENDPOINT: `${protocol}://${cluster}profiler.good-loop.com`,
+	// DATALOG_DATASPACE: 'gl',
+	ENDPOINT_NGO: 'https://test.sogive.org/charity',
+	// JUICE_ENDPOINT: 'https://localjuice.good-loop.com',
+	// ADRECORDER_ENDPOINT: 'http://localadrecorder.good-loop.com/record',
 };
