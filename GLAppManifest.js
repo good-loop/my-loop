e77a0853b09f480cc0f8f8a15828b26301f005f5
Roscoe <roscoe@good-loop.com>
2024-01-18 10:56:58 +0000

    Scrub all instances of Montserrat

--- GLAppManifest.js
+++ GLAppManifest.js
@@ -5,8 +5,8 @@ const GLAppManifest = {
 	name: "My.Good-Loop",
 	id: "my.good-loop.com",
 	dataspace: "good-loop",
-	logo: "/img/logo/my-loop-logo-round.svg",
-    logoMobile: "/img/logo/my-loop-logo-flat.svg",
+	logo: "/img/logo/My-Good-Loop-lockup.svg",
+	// logoMobile: "/img/logo/My-Good-Loop-lockup-vertical.svg",
 	website: "https://good-loop.com",	
 	facebookAppId: "320927325010346", // https://developers.facebook.com/apps/320927325010346/dashboard/
 	privacyPolicy: "https://doc.good-loop.com/privacy-policy.html",
