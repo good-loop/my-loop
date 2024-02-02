7fef916f7e2bf668e70d96426f3d8dacf93ea47b
Roscoe <roscoe@good-loop.com>
2024-02-02 17:35:32 +0000

    Maven cleanup & replace Jerbil dep with Apache FreeMarker

--- pages/green/index.md
+++
@@ -1,6 +0,0 @@
-title: Green Media powered by Good-Loop
-description: Carbon neutral advertising which funds tree-planting and other environmental action
-
-# Green Media powered by Good-Loop
-
-Carbon neutral advertising which funds tree-planting and other environmental action
--- src/java/com/goodloop/myloop/MetaHtmlServlet.java
+++ src/java/com/goodloop/myloop/MetaHtmlServlet.java
@@ -1,11 +1,14 @@
 package com.goodloop.myloop;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 
@@ -13,14 +16,10 @@ import com.goodloop.data.Advertiser;
 import com.goodloop.data.Agency;
 import com.goodloop.data.Campaign;
 import com.goodloop.data.NGO;
-import com.goodloop.jerbil.BuildJerbilPage;
-import com.goodloop.jerbil.JerbilConfig;
 import com.google.common.cache.CacheBuilder;
 import com.winterwell.data.AThing;
-import com.winterwell.utils.Dep;
 import com.winterwell.utils.Utils;
 import com.winterwell.utils.containers.Containers;
-import com.winterwell.utils.io.FileUtils;
 import com.winterwell.utils.log.Log;
 import com.winterwell.utils.time.Time;
 import com.winterwell.utils.web.WebUtils;
@@ -30,6 +29,10 @@ import com.winterwell.web.app.IServlet;
 import com.winterwell.web.app.ManifestServlet;
 import com.winterwell.web.app.WebRequest;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+
 /**
  * Make a meta data page for SEO
  * 
@@ -37,31 +40,56 @@ import com.winterwell.web.app.WebRequest;
  *
  */
 public class MetaHtmlServlet implements IServlet {
-
 	/**
-	 * NB: a file based cache would be faster (because: nginx) -- but this is faster
-	 * to handle data edits
+	 * NB: a file based cache would be faster (because: nginx)
+	 * -- but this is faster to handle data edits
 	 */
 	static ConcurrentMap<String, String> html4slug = (ConcurrentMap) CacheBuilder.newBuilder()
 			.expireAfterWrite(2, TimeUnit.MINUTES).concurrencyLevel(2).initialCapacity(10).build().asMap();
 
-	/** HACK "special" titles */
+
+	/** HACK Hardcoded page titles */
 	private static final Map<String, String> pageTitles;
-
-	static File template = new File("templates/template.html");
-
-	static String templateHtml = FileUtils.read(template);
-
 	static {
-		Map<String, String> aMap = new HashMap<String, String>();
-		aMap.put("home", "Raise money for charities simply by browsing the web");
-		aMap.put("impactoverview", "Impact Hub");
-		aMap.put("ihub", "Impact Hub");
-		aMap.put("green", "Green Media");
-		aMap.put("tabsforgood", "Sign Up for Tabs for Good");
-		aMap.put("ourimpact", "Our Impact");
-		aMap.put("getinvolved", "Get Involved");
-		pageTitles = Collections.unmodifiableMap(aMap);
+		Map<String, String> ptMap = new HashMap<String, String>();
+		ptMap.put("home", "Raise money for charities simply by browsing the web");
+		ptMap.put("impactoverview", "Impact Hub");
+		ptMap.put("ihub", "Impact Hub");
+		ptMap.put("tabsforgood", "Sign Up for Tabs for Good");
+		ptMap.put("ourimpact", "Our Impact");
+		ptMap.put("getinvolved", "Get Involved");
+		ptMap.put("green", "Green Media powered by Good-Loop");
+		ptMap.put("greendash", "Good-Loop: Green-Data Dashboard");
+		ptMap.put("ourstory", "My.Good-Loop: Our Story");
+		pageTitles = Collections.unmodifiableMap(ptMap);
+	}
+	/** ...and hardcoded page images */
+	private static final Map<String, String> pageImages;
+	static {
+		Map<String, String> imgMap = new HashMap<String, String>();
+		imgMap.put("ourstory", "/img/homepage/amyanddaniel.png");
+		pageImages = Collections.unmodifiableMap(imgMap);
+	}
+
+	/**
+	 * FreeMarker config - because this uses the package version, upgrading the Maven package can introduce breaking changes.
+	 * You can change this to e.g. Configuration.VERSION_2_3_32 if you upgrade the package, but need to lock config compatibility.
+	 * Probably file a ticket if you do this. 
+	 */
+	private static final Configuration freeMarkerConfig = new Configuration(Configuration.getVersion());	
+	static {
+		/** Boilerplate setup, see https://freemarker.apache.org/docs/pgui_quickstart_createconfiguration.html */	
+		try {
+			freeMarkerConfig.setDirectoryForTemplateLoading(new File("templates/"));
+		} catch (IOException e) {
+			throw new RuntimeException("Couldn't open templates directory?", e);
+		}
+		freeMarkerConfig.setDefaultEncoding("UTF-8");
+		freeMarkerConfig.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+		freeMarkerConfig.setLogTemplateExceptions(false);
+		freeMarkerConfig.setWrapUncheckedExceptions(true);
+		freeMarkerConfig.setFallbackOnNullLoopVariable(false);
+		freeMarkerConfig.setSQLDateAndTimeTimeZone(TimeZone.getDefault());
 	}
 
 	private String vertiserId;
@@ -107,10 +135,11 @@ public class MetaHtmlServlet implements IServlet {
 
 		// Construct endpoint URL and build client
 		String endpoint = protocol + "://" + prefix + "portal.good-loop.com/" + _class.getSimpleName().toLowerCase();
-		return new CrudClient(_class, endpoint);
+		return new CrudClient<T>(_class, endpoint);
 	}
 
-	private Map getPageSettings(WebRequest state, Map vars) {
+	
+	private Map<String, String> getPageSettings(WebRequest state, Map<String, String> vars) {
 		String bit0 = state.getSlugBits(0);
 		if ("campaign".equals(bit0) || "ihub".equals(bit0)) {
 			return getPageSettings2_impact(state, vars);
@@ -132,26 +161,38 @@ public class MetaHtmlServlet implements IServlet {
 			subtitle = subtitle.length() > 0 ? subtitle.substring(0, 1).toUpperCase() + subtitle.substring(1) : "";
 		}
 		vars.put("title", "My.Good-Loop " + subtitle);
-		vars.put("image", "");
+		// Page-specific image in metadata?
+		String image = pageImages.get(bit0);
+		if (image == null) image = "";
+		vars.put("image", image);
 
 		String loadingIMG = "img/splash-screen/background-loading.svg";
-		vars.put("contents", "<div style='height:100vh;background-color:#71ACBB;'><img src=" + loadingIMG
-				+ " alt='loading'/></div>");
+		vars.put("contents", "<div style=\"height:100vh;background-color:#71ACBB;\"><img src=\""
+				+ loadingIMG + "\" alt=\"loading\"/></div>");
 
 		// custom meta/SEO info?
 		// BlogPost??		
 		return vars;
 	}
 
-	private Map getPageSettings2_greendash(WebRequest state, Map vars) {
-		// do we have a campaign?
-//		if (campaignId==null) campaignId = state.getSlugBits(1);
-		Campaign campaign = null;
+	
+	/**
+	 * Sets:
+	 * title: "Good-Loop: Green-Data Dashboard" (+": $agencyName" if applicable)
+	 * description: "Good-Loop Green-Data Dashboard - Measure and reduce the carbon footprint from advertising"
+	 * image: Advertiser's background-image or logo, or agency's BG or logo
+	 * type: "summary"
+	 * @param state
+	 * @param vars
+	 * @return
+	 */
+	private Map<String, String> getPageSettings2_greendash(WebRequest state, Map<String, String> vars) {
+//		Campaign campaign = null;
 		Advertiser vertiser = null;
 		Agency agency = null;
-		if (campaignId!=null) {
-			campaign = (Campaign) getPortalClient(Campaign.class, state).getIfPresent(campaignId);
-		}
+//		if (campaignId != null) {
+//			campaign = (Campaign) getPortalClient(Campaign.class, state).getIfPresent(campaignId);
+//		}
 		if (agencyId!=null) {
 			agency = (Agency) getPortalClient(Agency.class, state).getIfPresent(agencyId);
 		}
@@ -159,29 +200,25 @@ public class MetaHtmlServlet implements IServlet {
 			vertiser = (Advertiser) getPortalClient(Advertiser.class, state).getIfPresent(vertiserId);
 		}
 
-		String companyName = "";
+		String title = "Good-Loop: Green-Data Dashboard";
+		// Append agency name to page title, if available
 		if (agency != null) {
-			companyName = agency.getName();
+			title += ": " + agency.getName();
 		}
-		
-		String description = "Good-Loop Green-Data Dashboard - Measure and reduce the carbon footprint from advertising";
-		String title = "Good-Loop: Green-Data Dashboard: "+companyName;
+		vars.put("title", WebUtils2.htmlEncode(title));
 
-		if (title != null)
-			vars.put("title", WebUtils2.htmlEncode(title));
-		
+		String description = "Good-Loop Green-Data Dashboard - Measure and reduce the carbon footprint from advertising";
+		vars.put("description", WebUtils2.htmlEncode(description));
+
 		// image
 		String img = "";
-		if (agency != null && agency.getBranding()!=null) {
-			img = Utils.or(agency.getBranding().backgroundImage, agency.getBranding().logo);
-		}
 		if (vertiser!= null && vertiser.getBranding()!=null) {
 			img = Utils.or(vertiser.getBranding().backgroundImage, vertiser.getBranding().logo);
+		} else if (agency != null && agency.getBranding()!=null) {
+			img = Utils.or(agency.getBranding().backgroundImage, agency.getBranding().logo);
 		}
 		vars.put("image", img);
 		
-		vars.put("description", WebUtils2.htmlEncode(description));
-		vars.put("type", "summary");
 		return vars;
 	}
 
@@ -201,17 +238,22 @@ public class MetaHtmlServlet implements IServlet {
 		}
 	}
 
+
 	/**
-	 * 
+	 * Sets:
+	 * title: "Good-Loop for $charityName" or fallback to "/charity/$charityID"
+	 * image: $charity.getPhoto() or fallback to Good-Loop logo
+	 * contents: ""
+	 * type: "summary"
 	 * @param state
 	 * @param vars
 	 * @return vars (same object as input)
 	 */
-	private Map getPageSettings2_charity(WebRequest state, Map vars) {
+	private Map<String, String> getPageSettings2_charity(WebRequest state, Map<String, String> vars) {
 		vars.put("title", state.getRequestPath());
-		vars.put("image", "https://good-loop.com/img/logo/good-loop-logo-text.png");
+		vars.put("image", "https://my.good-loop.com/img/logo/Good-Loop-lockup-vertical.svg");
 		vars.put("contents", "");
-		// do we have a campaign?
+		// do we have a charity?
 		String cid = state.getSlugBits(1);
 		if (cid != null) {
 			CrudClient<NGO> cc = new CrudClient<NGO>(NGO.class, "https://app.sogive.org/charity");
@@ -227,7 +269,18 @@ public class MetaHtmlServlet implements IServlet {
 		return vars;
 	}
 
-	private Map getPageSettings2_impact(WebRequest state, Map vars) {
+
+	/**
+	 * Sets:
+	 * title: "Good-Loop Impact: $brand"
+	 * image: Campaign background-image URL
+	 * description: "See the impact $brand has had with Good-Loop ethical advertising!"
+	 * type: "summary"
+	 * @param state
+	 * @param vars
+	 * @return
+	 */
+	private Map<String, String> getPageSettings2_impact(WebRequest state, Map<String, String> vars) {
 		// do we have a campaign?
 		String cid = state.getSlugBits(1);
 		Campaign campaign = null;
@@ -253,6 +306,7 @@ public class MetaHtmlServlet implements IServlet {
 				companyName = agency.name;
 			}
 		}
+
 		if (cid != null) {
 			CrudClient<Campaign> cc = getPortalClient(Campaign.class, state);
 			campaign = cc.get(cid).java();
@@ -264,7 +318,6 @@ public class MetaHtmlServlet implements IServlet {
 					vertiser = cc2.get(campaign.vertiser).java();
 					if (vertiser != null) {
 						companyName = vertiser.name;
-						System.out.println("VERTISER: " + campaign.vertiser + " NAME: " + vertiser.name);
 					}
 				} catch (Exception e) {
 					// do nothing
@@ -282,72 +335,57 @@ public class MetaHtmlServlet implements IServlet {
 			title = "Good-Loop Impact: Ads for Good";
 		}
 
-		if (title != null)
-			vars.put("title", WebUtils2.htmlEncode(title));
-		if (campaign != null)
-			vars.put("image", campaign.bg);
-		if (description != null)
-			vars.put("description", WebUtils2.htmlEncode(description));
-		vars.put("type", "summary");
+		if (title != null) vars.put("title", WebUtils2.htmlEncode(title));
+		if (campaign != null) vars.put("image", campaign.bg);
+		if (description != null) vars.put("description", WebUtils2.htmlEncode(description));
 		return vars;
 	}
 
+
 	@Override
 	public void process(WebRequest state) throws Exception {
+		// Construct a cache key for the visited page and agency/advertiser/campaign context
 		String slug = state.getSlug();
-		if (slug == null)
-			slug = "null"; // so we can cache it anyway
+		if (slug == null) slug = "null"; // so we can cache it anyway
 
 		// For impact hub, we want different caches per vertiser/agency
 		vertiserId = Utils.or(state.get("gl.vertiser"), state.get("brand"));
 		agencyId = Utils.or(state.get("gl.agency"), state.get("agency"));
 		campaignId = state.get("campaign");
-		if (vertiserId != null)
-			slug += "_vertiser:" + vertiserId;
-		if (agencyId != null)
-			slug += "_agency:" + agencyId;
 		String serverType = state.get("server");
-		if (serverType != null)
-			slug += "_server:" + serverType;
+		if (vertiserId != null) slug += "_vertiser:" + vertiserId;
+		if (agencyId != null) slug += "_agency:" + agencyId;
+		if (serverType != null) slug += "_server:" + serverType;
+		
+
 
 		String html = html4slug.get(slug);
 		if (html == null || state.debug) {
-			// not cached - so make it
-			JerbilConfig jc = new JerbilConfig();
-			jc.useJS = false;
-			Dep.set(JerbilConfig.class, jc);
-
-			// make the page and cache it
-			File src = new File("pages", FileUtils.safeFilename(state.getSlug() + ".md"));
-			String srcText = "";
-			if (src.isFile()) {
-				srcText = FileUtils.read(src);
-			} else {
-				src = null;
-			}
-			File out = null;
-			BuildJerbilPage bjp = new BuildJerbilPage(src, "", template, out);
-
 			// TODO fill in the SEO and social stuff from file or API
-			Map<String,String> vars = new HashMap();
-			// Always replace $contents and $title placeholder in HTML
+			Map<String,String> vars = new HashMap<String, String>();
+			// Always have a value for ${contents}, ${title} and ${type}
 			vars.put("contents", "");
 			vars.put("title", "Good-Loop");
 
+			// Page-specific overrides for title etc
 			try {
 				getPageSettings(state, vars);
 			} catch (Throwable e) {
 				// swallow it, more or less. Probably a 404
 				Log.w("MetaHtml", e+" from "+state);
 			}
-			// cachebuster		
+			// cachebuster
 			vars.put("cachebuster", cachebuster());
 			// sanitise inputs against attack
-			Map safeVars = Containers.applyToValues(s -> WebUtils2.stripScripts(s), vars);
-			// Jerbil it!
-			String pageHtml = bjp.run2_render(false, srcText, templateHtml, safeVars);
+			Map<String, String> safeVars = Containers.applyToValues(s -> WebUtils2.stripScripts(s), vars);
+			
+			// Generate page
+			Template basePage = freeMarkerConfig.getTemplate("template.ftlh");
+			StringWriter sw = new StringWriter();
+			basePage.process(safeVars, sw);
+
 			// cache it
-			html = pageHtml;
+			html = sw.toString();
 			html4slug.put(slug, html);
 		}
 		// send it back
