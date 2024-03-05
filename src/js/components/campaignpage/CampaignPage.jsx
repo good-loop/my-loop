743009c36fe93869afef5b9ae4c9a54e910e9f85
athomson0 <aidan@good-loop.com>
2024-03-05 13:15:04 +0000

    donation: 50% -> 20% to meet legal requirements

--- src/js/components/campaignpage/CampaignPage.jsx
+++ src/js/components/campaignpage/CampaignPage.jsx
@@ -391,7 +391,7 @@ const SmallPrintInfo = ({ads, charities, campaign, pvTopItem}) => {
 					{dmin && (Money.value(dmin) || Money.value(dmax))?  /* NB: using && here resulted in a stray "0" */
 						<>Donation Amount: <Misc.Money amount={dmin} /> { dmax &&!Money.eq(dmin,dmax) && <> to <Misc.Money amount={dmax} /></>} per video viewed <br/></>
 					: null}
-					{campaign.id === "zeni_aviv_8412" ? "20%" : "50%"} of the advertising cost for each advert is donated. Most of the rest goes to pay the publisher and related companies.
+					20% of the advertising cost for each advert is donated. Most of the rest goes to pay the publisher and related companies.
 					Good-Loop and the advertising exchange make a small commission. The donations depend on viewers watching the adverts.
 				</span>
 				<br/>
